f955d270e65470f2aaf5ff8c5dcfe1408a4bd85c
Chicherov Vlad <vlad@sirena2000.ru>
2006-08-21 14:45:25 +0000

    *** empty log message ***
    git-svn-id: svn+ssh://10.1.90.223/SVNroot/astra/server/trunk@66604 e9a6705e-e829-0410-83f2-cebdff2db7e5

---
+++ checkin.h
@@ -0,0 +1,27 @@
+#ifndef _CHECKIN_H_
+#define _CHECKIN_H_
+
+#include <libxml/tree.h>
+#include "JxtInterface.h"		
+
+class CheckInInterface : public JxtInterface
+{
+public:
+  CheckInInterface() : JxtInterface("","CheckIn")
+  {
+     Handler *evHandle;
+     evHandle=JxtHandler<CheckInInterface>::CreateHandler(&CheckInInterface::SavePax);
+     AddEvent("SavePax",evHandle);     
+     AddEvent("kick",JxtHandler<CheckInInterface>::CreateHandler(&CheckInInterface::KickHandler));
+  };	
+  
+  void SavePax(XMLRequestCtxt *ctxt, xmlNodePtr reqNode, xmlNodePtr resNode);
+  void KickHandler(XMLRequestCtxt *ctxt, xmlNodePtr reqNode, xmlNodePtr resNode);
+  virtual void Display(XMLRequestCtxt *ctxt, xmlNodePtr reqNode, xmlNodePtr resNode) {};  
+};
+
+typedef enum {csaFlt,csaGrp,csaPax} TETCheckStatusArea;
+
+bool ETCheckStatus(int id, TETCheckStatusArea area, int point_id=-1);
+
+#endif
