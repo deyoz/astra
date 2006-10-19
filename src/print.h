eaaf576f4a8d3edee4a6f5f714701f17b35da3ad
Denis Zaharaov <denis_zaharov@sirena2000.ru>
2006-10-19 09:00:33 +0000

    confirm print handlers
    git-svn-id: svn+ssh://10.1.90.223/SVNroot/astra/server/trunk@66744 e9a6705e-e829-0410-83f2-cebdff2db7e5

--- src/print.h
+++ src/print.h
@@ -14,20 +14,23 @@ class PrintInterface: public JxtInterface
         PrintInterface(): JxtInterface("123", "print")
         {
             Handler *evHandle;
-            evHandle=JxtHandler<PrintInterface>::CreateHandler(&PrintInterface::GetPectabDataBP);
-            AddEvent("GetPectabDataBP",evHandle);
             evHandle=JxtHandler<PrintInterface>::CreateHandler(&PrintInterface::GetPrintDataBPXML);
             AddEvent("GetPrintDataBP",evHandle);
             evHandle=JxtHandler<PrintInterface>::CreateHandler(&PrintInterface::GetGRPPrintDataBPXML);
             AddEvent("GetGRPPrintDataBP",evHandle);
             evHandle=JxtHandler<PrintInterface>::CreateHandler(&PrintInterface::GetPrintDataBTXML);
             AddEvent("GetPrintDataBT",evHandle);
+            evHandle=JxtHandler<PrintInterface>::CreateHandler(&PrintInterface::ConfirmPrintBT);
+            AddEvent("ConfirmPrintBT",evHandle);
+            evHandle=JxtHandler<PrintInterface>::CreateHandler(&PrintInterface::ConfirmPrintBP);
+            AddEvent("ConfirmPrintBP",evHandle);
         }
 
-        void GetPectabDataBP(XMLRequestCtxt *ctxt, xmlNodePtr reqNode, xmlNodePtr resNode);
         void GetGRPPrintDataBPXML(XMLRequestCtxt *ctxt, xmlNodePtr reqNode, xmlNodePtr resNode);
         void GetPrintDataBPXML(XMLRequestCtxt *ctxt, xmlNodePtr reqNode, xmlNodePtr resNode);
         void GetPrintDataBTXML(XMLRequestCtxt *ctxt, xmlNodePtr reqNode, xmlNodePtr resNode);
+        void ConfirmPrintBT(XMLRequestCtxt *ctxt, xmlNodePtr reqNode, xmlNodePtr resNode);
+        void ConfirmPrintBP(XMLRequestCtxt *ctxt, xmlNodePtr reqNode, xmlNodePtr resNode);
         virtual void Display(XMLRequestCtxt *ctxt, xmlNodePtr reqNode, xmlNodePtr resNode);  
 };
 
