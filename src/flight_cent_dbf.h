66b2b67e1dc140dd238c3313db41a6bf2aee63fb
cvsreader <cvsreader@sirena2000.ru>
2007-06-17 16:34:54 +0000

    *** empty log message ***
    git-svn-id: svn+ssh://10.1.90.223/SVNroot/astra/server/trunk@67173 e9a6705e-e829-0410-83f2-cebdff2db7e5

--- src/flight_cent_dbf.h
+++ src/flight_cent_dbf.h
@@ -4,6 +4,8 @@
 #include <string>
 #include <map>
 
+const std::string FILE_CENT_TYPE = "CENTR";
+
 bool createCentringFile( int point_id, std::map<std::string,std::string> &params, std::string &file_data  );
 
 #endif /* cent_dbfH */
