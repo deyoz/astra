f1e2ef601f09f1a0197d13fbd82eb4dd0aa200b3
cvsreader <cvsreader@sirena2000.ru>
2007-07-31 11:45:47 +0000

    2 new consts
    git-svn-id: svn+ssh://10.1.90.223/SVNroot/astra/server/trunk@67235 e9a6705e-e829-0410-83f2-cebdff2db7e5

--- src/develop_dbf.h
+++ src/develop_dbf.h
@@ -5,6 +5,8 @@
 #include <sstream>
 #include <vector>
 
+const std::string PARAM_PULT = "PULT";
+const std::string PARAM_CANON_NAME = "CANON_NAME";
 const std::string PARAM_WORK_DIR = "WORKDIR";
 const std::string PARAM_FILE_NAME = "FileName";
 const std::string PARAM_IN_ORDER = "IN_ORDER";	
