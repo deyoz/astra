845d092573e9f500819bf942b428a25140b035d1
cvsreader <cvsreader@sirena2000.ru>
2007-07-31 11:24:24 +0000

    reformat
    git-svn-id: svn+ssh://10.1.90.223/SVNroot/astra/server/trunk@67231 e9a6705e-e829-0410-83f2-cebdff2db7e5

--- src/sofi.cc
+++ src/sofi.cc
@@ -28,21 +28,33 @@ void createFileParamsSofi( int receipt_id, map<string,string> &params )
 	//!!! надо считать
 	TQuery Qry( &OraSession );
 	Qry.SQLText = 
+     "declare "
+     "  update_time date; "
 	 "BEGIN "
-	 " SELECT n INTO :nnn FROM sofi; "
-	 " UPDATE sofi SET n=:nnn+1; "
+	 " SELECT n, update_time INTO :nnn, update_time FROM sofi "
+     "  where canon_name = :canon_name for update; "
+     " if trunc(update_time) <> trunc(sysdate) then "
+     "   delete from sofi where canon_name = :canon_name; "
+     " end if; "
+	 " UPDATE sofi SET n=:nnn+1 where canon_name = :canon_name; "
 	 " EXCEPTION WHEN NO_DATA_FOUND THEN "
 	 "   :nnn := 0; "
-	 "   INSERT INTO sofi VALUES( 1 ); "
+	 "   INSERT INTO sofi(n, canon_name, update_time) VALUES( 1, :canon_name, sysdate ); "
 	 "END; ";
 	Qry.CreateVariable( "nnn", otInteger, 0 );
+	Qry.CreateVariable( "canon_name", otString, params[PARAM_CANON_NAME] );
 	tst();
 	Qry.Execute();
 	tst();
 	ostringstream res;
-	res<<setw(0)<<DateTimeToStr(NowUTC(),"dd.mm.yyyy");
-	res<<setfill('0')<<setw(3)<<Qry.GetVariableAsInteger( "nnn" );
-	res<<setw(0)<<".txt";
+	res <<setw(0)<<DateTimeToStr(NowUTC(),"yymmdd");
+    res << params[PARAM_PULT];
+	res <<Qry.GetVariableAsInteger( "nnn" );
+	res <<setw(0)<<".txt";
+    ProgTrace(TRACE5, "params.size = %d", params.size());
+    for(map<string,string>::iterator im = params.begin(); im != params.end(); im++) {
+        ProgTrace(TRACE5, "params[%s] = %s", im->first.c_str(), im->second.c_str());
+    }
   params[ PARAM_FILE_NAME ] =  res.str();
 }
 
@@ -52,12 +64,12 @@ bool createSofiFile( int receipt_id, std::map<std::string,std::string> &params, 
   TQuery Qry(&OraSession);
   Qry.SQLText=
     "SELECT TO_CHAR(bag_receipts.no) no,bag_receipts.form_type,bag_receipts.aircode,bag_receipts.issue_date, "
-    "      bag_receipts.pax_name,bag_receipts.airp_dep,bag_receipts.airp_arv, "
+    "      bag_receipts.pax_name,bag_receipts.tickets,bag_receipts.airp_dep,bag_receipts.airp_arv, "
     "      bag_receipts.airline,bag_receipts.flt_no,bag_receipts.suffix,bag_receipts.grp_id,bag_receipts.ex_weight,  "
     "      bag_receipts.rate,bag_receipts.rate_cur, "
     "      bag_receipts.exch_rate,bag_receipts.exch_pay_rate,bag_receipts.pay_rate_cur, "
     "      bag_receipts.ex_weight,bag_receipts.issue_user_id,  "
-    "      desks.sale_point as sale_point, sale_points.city as sale_city,  "
+    "      desks.code pult,desks.sale_point as sale_point, sale_points.city as sale_city,  "
     "      points.scd_out, users2.descr  "
     "FROM bag_receipts, desks, sale_points, pax_grp, points, users2  "
     "WHERE receipt_id= :id AND  "
@@ -79,49 +91,57 @@ bool createSofiFile( int receipt_id, std::map<std::string,std::string> &params, 
 	res<<"_BAGGAGE_";
 	res<<dlmt; //2 Служебная информация CHAR(X)
 	string t = Qry.FieldAsString( "form_type" );
-	if ( t == "M61" )
-		res<<setw(3)<<"M";
-	else
-		if ( t == "Z61" )
-			res<<setw(3)<<"Z";
-		else
-			if ( t == "35" )
-				res<<setw(3)<<" "; //???
-			else
-				res<<setw(3)<<" "; //???
+    res<<setw(3)<<t;
  res<<dlmt; //3 Серия бланка Z, M, CHA, CБА CHAR(3)
- res<<setw(10)<<string(Qry.FieldAsString( "no" )).substr(0,10);
+ res<<trim(string(Qry.FieldAsString( "no" )).substr(0,10));
  res<<dlmt; //4 Номер бланка NUMBER(10)
- res<<setw(3)<<Qry.FieldAsString( "aircode" );
+ res<<Qry.FieldAsString( "aircode" );
  res<<dlmt; //5 Расчетный код CHAR(3)
  string val = CityTZRegion( Qry.FieldAsString( "sale_city" ) );
  TDateTime d = UTCToLocal( Qry.FieldAsDateTime( "issue_date" ), val );
- res<<setw(10)<<DateTimeToStr( d, "dd.mm.yyyy");
+ res<<DateTimeToStr( d, "dd.mm.yyyy");
  res<<dlmt; //6 Дата продажи CHAR(10) 10.10.2006
- res<<setw(50)<<Qry.FieldAsString( "pax_name" );
- res<<dlmt; //6
+
+ // surname & name
+ {
+     string pax_name = trim(string(Qry.FieldAsString( "pax_name" )));
+     string surname;
+     string::size_type pos = pax_name.find(" ");
+     if(pos != string::npos) {
+         surname = pax_name.substr(0, pos);
+         pax_name = pax_name.substr(pos + 1);
+     }
+     res<<trim(surname.substr(0, 50));
+     res<<dlmt;
+     res<<trim(pax_name.substr(0, 50));
+     res<<dlmt; //6
+ }
+
+ res<<trim(string(Qry.FieldAsString( "tickets" )).substr(0, 13));
+ res<<dlmt;
+
  TAirpsRow *row=(TAirpsRow*)&base_tables.get("airps").get_row("code",Qry.FieldAsString("airp_dep"));
  if ( row->city.empty() )
-   res<<setw(3)<<Qry.FieldAsString("airp_dep"); //???4
+   res<<Qry.FieldAsString("airp_dep"); //???4
  else
- 	 res<<setw(3)<<row->city; //???4
+ 	 res<<row->city; //???4
  res<<dlmt; //7
  row=(TAirpsRow*)&base_tables.get("airps").get_row("code",Qry.FieldAsString("airp_arv"));
  if ( row->city.empty() )
-   res<<setw(3)<<Qry.FieldAsString("airp_arv"); //???4
+   res<<Qry.FieldAsString("airp_arv"); //???4
  else
- 	 res<<setw(3)<<row->city; 	//???4
+ 	 res<<row->city; 	//???4
  res<<dlmt; //8
- res<<setw(3)<<Qry.FieldAsString( "airline" );
+ res<<trim(string(Qry.FieldAsString( "airline" )));
  res<<dlmt; //9
- res<<setw(6)<<string(Qry.FieldAsString( "flt_no" )) + Qry.FieldAsString( "suffix" );
+ res<<trim(string(Qry.FieldAsString( "flt_no" )) + Qry.FieldAsString( "suffix" ));
  res<<dlmt; //10
  val = AirpTZRegion( Qry.FieldAsString("airp_dep") );
  d = UTCToLocal( Qry.FieldAsDateTime( "scd_out" ), val );
- res<<setw(10)<<DateTimeToStr( d, "dd.mm.yyyy");
+ res<<DateTimeToStr( d, "dd.mm.yyyy");
  res<<dlmt; //11
- res<<setprecision(2);
- res<<setw(11)<<Qry.FieldAsInteger( "ex_weight" ); //???9.3(2)
+ res << setprecision(2);
+ res<<trim(IntToString(Qry.FieldAsInteger( "ex_weight" ))); //???9.3(2)
  res<<dlmt; //12
 
  //короче, баклан
@@ -152,14 +172,20 @@ bool createSofiFile( int receipt_id, std::map<std::string,std::string> &params, 
  else
    pay_rate = Qry.FieldAsFloat( "rate" );
 
- res<<setw(12)<<pay_rate; //???10.3(2) "rate" - валюта тарифа, pay_rate - валюта оплаты
+ ostringstream buf;
+ buf << std::fixed << setprecision(2) << pay_rate;
+
+ res<<trim(buf.str().substr(0, 10)); //???10.3(2) "rate" - валюта тарифа, pay_rate - валюта оплаты
  res<<dlmt; //13
- res<<pay_rate*Qry.FieldAsInteger( "ex_weight" ); //???12.3
+
+ buf.str("");
+ buf<<pay_rate*Qry.FieldAsInteger( "ex_weight" ); //???12.3
+ res<<trim(buf.str().substr(0, 12)); //???10.3(2) "rate" - валюта тарифа, pay_rate - валюта оплаты
  res<<dlmt<<setprecision(0);
- res<<setw(20)<<Qry.FieldAsString( "descr" ); //???20 а может надо login
+ res<<Qry.FieldAsString( "pult" );
+ params[PARAM_PULT] = Qry.FieldAsString( "pult" );
  res<<dlmt; //15
- res<<setw(10)<<Qry.FieldAsString( "sale_point" )<<setw(0);
- //res<<'\t'; //16
+ res<<dlmt; //15
  file_data = res.str();
  createFileParamsSofi( receipt_id, params );
  return true;
