5650d13b998b5153ef9628d279fb62e943571d73
cvsreader <cvsreader@sirena2000.ru>
2008-07-03 21:27:35 +0000

    Merged revisions 78840 via svnmerge from svn+ssh://svn/SVNroot/astra/server/trunk
    ........
      r78840 | cvsreader | 2008-07-04 01:23:10 +0400 (Fri, 04 Jul 2008) | 2 lines
      
      Kupol query format for OMS added
    ........
    
    
    git-svn-id: svn+ssh://10.1.90.223/SVNroot/astra/server/stable@78842 e9a6705e-e829-0410-83f2-cebdff2db7e5

--- src/checkin.cc
+++ src/checkin.cc
@@ -852,7 +852,8 @@ void CheckInInterface::SearchPax(XMLRequestCtxt *ctxt, xmlNodePtr reqNode, xmlNo
     if (reqInfo->desk.city=="‘“"||
         reqInfo->desk.city=="Š‡"||
         reqInfo->desk.city=="‘•„"||
-        reqInfo->desk.city=="†‚")
+        reqInfo->desk.city=="†‚"||
+        reqInfo->desk.city=="ŽŒ‘")
     {
       fmt.persCountFmt=1;
       fmt.infSeatsFmt=1;
