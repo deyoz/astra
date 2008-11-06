68cd12a7f80f6a208ee28cc5da933a32fc572911
Anton Bokov <a.bokov@sirena2000.ru>
2008-11-06 16:01:11 +0000

    header for xp_testing
    AM + xp_testing.h
    
    
    git-svn-id: svn+ssh://10.1.90.223/SVNroot/astra/server/trunk@84605 e9a6705e-e829-0410-83f2-cebdff2db7e5

---
+++ src/xp_testing.h
@@ -0,0 +1,12 @@
+#ifndef __XP_TESTING_H__
+#define __XP_TESTING_H__
+#include "config.h"
+
+#ifdef XP_TESTING
+#include "xp_test_utils.h"
+#include "checkunit.h"
+
+void testClearShutDB();
+#endif /*XP_TESTING*/
+
+#endif /* __XP_TEST_UTILS_H__ */
