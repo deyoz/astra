24359af9f13bc6297733be9245c8543babf15e68
Roman Kovalev <rom@sirena2000.ru>
2009-02-10 06:24:12 +0000

    !!!!!!!!!!!!!!!!!!!!!!!!111один
    git-svn-id: svn+ssh://10.1.90.223/SVNroot/astra/server/trunk@88902 e9a6705e-e829-0410-83f2-cebdff2db7e5

--- src/tlg/edi_tlg.cpp
+++ src/tlg/edi_tlg.cpp
@@ -967,7 +967,7 @@ int CreateEDIREQ (edi_mes_head *pHead, void *udata, void *data, int *err)
         // Из второй запись в БД
         const message_funcs_type &mes_funcs=
                 EdiMesFuncs::GetEdiFunc(pHead->msg_type, ed->msgId());
-        ed->sessDataWr()->SetEdiSessMesAttrOnly();
+        ed->sessDataWr()->SetEdiSessMesAttr();
         // Создает стр-ру EDIFACT
         if(::CreateMesByHead(ed->sessData()->edih()))
         {
