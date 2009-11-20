b381ba01dc0df547a53d1dd3e8b20b20c47943ff
Denis Zaharaov <denis_zaharov@sirena2000.ru>
2009-11-20 12:59:25 +0000

    deprecate code removed
    git-svn-id: svn+ssh://10.1.90.223/SVNroot/astra/server/trunk@101252 e9a6705e-e829-0410-83f2-cebdff2db7e5

--- src/docs.h
+++ src/docs.h
@@ -6,7 +6,6 @@
 #include "astra_consts.h"
 #include "basic.h"
 
-void RunRpt(std::string name, xmlNodePtr reqNode, xmlNodePtr resNode);
 void get_report_form(const std::string name, xmlNodePtr node);
 void PaxListVars(int point_id, int pr_lat, xmlNodePtr variablesNode,
                  BASIC::TDateTime part_key = ASTRA::NoExists);
@@ -20,34 +19,16 @@ public:
   DocsInterface() : JxtInterface("","docs")
   {
      Handler *evHandle;
-     evHandle=JxtHandler<DocsInterface>::CreateHandler(&DocsInterface::SaveForm);
-     AddEvent("save_form",evHandle);
-     evHandle=JxtHandler<DocsInterface>::CreateHandler(&DocsInterface::LoadForm);
-     AddEvent("load_form",evHandle);
      evHandle=JxtHandler<DocsInterface>::CreateHandler(&DocsInterface::SaveReport);
      AddEvent("save_report",evHandle);
-     evHandle=JxtHandler<DocsInterface>::CreateHandler(&DocsInterface::RunReport);
-     AddEvent("run_report",evHandle);
      evHandle=JxtHandler<DocsInterface>::CreateHandler(&DocsInterface::RunReport2);
      AddEvent("run_report2",evHandle);
-     evHandle=JxtHandler<DocsInterface>::CreateHandler(&DocsInterface::GetFltInfo);
-     AddEvent("GetFltInfo",evHandle);
-     evHandle=JxtHandler<DocsInterface>::CreateHandler(&DocsInterface::GetSegList);
-     AddEvent("GetSegList",evHandle);
-     evHandle=JxtHandler<DocsInterface>::CreateHandler(&DocsInterface::GetSegList2);
-     AddEvent("GetSegList2",evHandle);
      evHandle=JxtHandler<DocsInterface>::CreateHandler(&DocsInterface::GetFonts);
      AddEvent("GetFonts",evHandle);
   };
 
-  void SaveForm(XMLRequestCtxt *ctxt, xmlNodePtr reqNode, xmlNodePtr resNode);
-  void LoadForm(XMLRequestCtxt *ctxt, xmlNodePtr reqNode, xmlNodePtr resNode);
   void SaveReport(XMLRequestCtxt *ctxt, xmlNodePtr reqNode, xmlNodePtr resNode);
-  void RunReport(XMLRequestCtxt *ctxt, xmlNodePtr reqNode, xmlNodePtr resNode);
   void RunReport2(XMLRequestCtxt *ctxt, xmlNodePtr reqNode, xmlNodePtr resNode);
-  void GetFltInfo(XMLRequestCtxt *ctxt, xmlNodePtr reqNode, xmlNodePtr resNode);
-  void GetSegList(XMLRequestCtxt *ctxt, xmlNodePtr reqNode, xmlNodePtr resNode);
-  void GetSegList2(XMLRequestCtxt *ctxt, xmlNodePtr reqNode, xmlNodePtr resNode);
   void GetFonts(XMLRequestCtxt *ctxt, xmlNodePtr reqNode, xmlNodePtr resNode);
   static void GetZoneList(int point_id, xmlNodePtr dataNode);
   virtual void Display(XMLRequestCtxt *ctxt, xmlNodePtr reqNode, xmlNodePtr resNode) {};
