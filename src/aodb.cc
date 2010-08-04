c709296990dc31f93ce70512e57b3698ac30a2bd
jen <jen@sirena2000.ru>
2010-08-04 10:57:11 +0000

    new term aodb format
    git-svn-id: svn+ssh://10.1.90.223/SVNroot/astra/server/trunk@112459 e9a6705e-e829-0410-83f2-cebdff2db7e5

--- src/aodb.cc
+++ src/aodb.cc
@@ -2020,8 +2020,8 @@ bool BuildAODBTimes( int point_id, const std::string &point_addr, TFileDatas &fd
     string term = StationsQry.FieldAsString( "name" );
     if ( !term.empty() && term[0] == 'R' )
     	term = term.substr( 1, term.length() - 1 );
-    if ( !term.empty() && term[0] == '0' )
-    	term = term.substr( 1, term.length() - 1 );
+/*!!!den//!!!04.08.2010    if ( !term.empty() && term[0] == '0' )
+    	term = term.substr( 1, term.length() - 1 );*/
 		record<<";"<<""<<setw(4)<<term.substr(0,4)<<setw(1)<<(int)!StationsQry.FieldIsNULL( "start_time" );
 		StationsQry.Next();
 	}
