f8e179614f095258223a38479f5a12281ac465e5
Chicherov Vlad <vlad@sirena2000.ru>
2010-09-15 11:38:02 +0000

    astra_elems.cc added. Not tested
    git-svn-id: svn+ssh://10.1.90.223/SVNroot/astra/server/trunk@113907 e9a6705e-e829-0410-83f2-cebdff2db7e5

---
+++ src/astra_elems.cc
@@ -0,0 +1,725 @@
+#include <string>
+#include <sstream>
+
+#include "astra_elems.h"
+#include "astra_consts.h"
+#include "astra_utils.h"
+#include "base_tables.h"
+#include "exceptions.h"
+
+#define NICKNAME "VLAD"
+#include "serverlib/test.h"
+
+using namespace std;
+using namespace EXCEPTIONS;
+
+const char* EncodeElemContext(const TElemContext ctxt)
+{
+  switch( ctxt ) {
+		case ecDisp:
+		  return "ecDisp";
+		case ecCkin:
+		  return "ecCkin";
+		case ecTrfer:
+		  return "ecTrfer";
+		case ecTlgTypeB:
+		  return "ecTlgTypeB";
+		case ecNone:
+		  return "ecNone";
+	}
+	return "";
+};
+
+const char* EncodeElemType(const TElemType type)
+{
+  switch( type ) {
+  	case etCountry:
+  		return "etCountry";
+  	case etCity:
+  		return "etCity";
+  	case etAirline:
+  		return "etAirline";
+  	case etAirp:
+  		return "etAirp";
+  	case etCraft:
+  		return "etCraft";
+  	case etClass:
+  		return "etClass";
+  	case etSubcls:
+  		return "etSubcls";
+  	case etPersType:
+  		return "etPersType";
+  	case etGenderType:
+  		return "etGenderType";
+  	case etPaxDocType:
+  		return "etPaxDocType";
+  	case etPayType:
+  		return "etPayType";
+  	case etCurrency:
+  		return "etCurrency";
+  	case etRefusalType:
+  	  return "etRefusalType";
+    case etSuffix:
+  		return "etSuffix";
+  	case etTripType:
+  		return "etTripType";
+  	case etCompElemType:
+  		return "etCompElemType";
+  	case etGrpStatusType:
+  		return "etGrpStatusType";
+  	case etClientType:
+  		return "etClientType";
+  	case etCompLayerType:
+  		return "etCompLayerType";
+  	case etCrs:
+  		return "etCrs";
+  	case etDevModel:
+  		return "etDevModel";
+    case etDevSessType:
+  		return "etDevSessType";
+    case etDevFmtType:
+  		return "etDevFmtType";
+  	case etDevOperType:
+  		return "etDevOperType";
+  	case etGraphStage:
+  		return "etGraphStage";
+  }
+  return "";
+};
+
+void DoElemEConvertError( TElemContext ctxt, TElemType type, const string &elem )
+{
+	ostringstream msg;
+	msg << "Can't convert elem to id: "
+	    << "ctxt=" << EncodeElemContext(ctxt) << ", "
+	    << "type=" << EncodeElemType(type) << ", "
+	    << "elem=" << elem;
+
+  throw EConvertError( msg.str().c_str() );
+}
+
+string ElemCtxtToElemId(TElemContext ctxt,TElemType type, string code, int &fmt,
+                        bool hard_verify, bool with_deleted)
+{
+  string id;
+  fmt=efmtUnknown;
+
+  if (code.empty()) return id;
+
+  TElemFmt elemFmt=(TElemFmt)fmt;
+  id = ElemToElemId(type,code,elemFmt,"",with_deleted);
+
+  //далее проверим а вообще имели ли мы право вводить в таком формате
+  if ( hard_verify ) {
+    if (ctxt==ecTlgTypeB && (type!=etCountry && fmt!=0 && fmt!=1 ||
+                             type==etCountry && fmt!=0 && fmt!=1 && fmt!=4) /*||
+        ctxt==ecCkin && (fmt!=0) ||
+        ctxt==ecTrfer && (fmt!=0)!!!vlad*/)
+    {
+      //проблемы
+      DoElemEConvertError( ctxt, type, code );
+    };
+  }
+  if ( ctxt==ecDisp || ctxt==ecCkin )
+  {
+    if(type==etAirline ||
+       type==etAirp ||
+       type==etCraft ||
+       type==etSuffix)
+    {
+      TReqInfo *reqInfo = TReqInfo::Instance();
+      TUserSettingType user_fmt;
+      switch(ctxt) {
+      	case ecDisp:
+          if (type==etAirline) user_fmt=reqInfo->user.sets.disp_airline;
+          if (type==etAirp) user_fmt=reqInfo->user.sets.disp_airp;
+          if (type==etCraft) user_fmt=reqInfo->user.sets.disp_craft;
+          if (type==etSuffix) user_fmt=reqInfo->user.sets.disp_suffix;
+          break;
+      	case ecCkin:
+          if (type==etAirline) user_fmt=reqInfo->user.sets.disp_airline;
+          if (type==etAirp) user_fmt=reqInfo->user.sets.disp_airp;
+          if (type==etCraft) user_fmt=reqInfo->user.sets.disp_craft;
+          if (type==etSuffix) user_fmt=reqInfo->user.sets.disp_suffix;
+          break;
+        default:;
+      }
+      if (type==etAirline) user_fmt=reqInfo->user.sets.disp_airline;
+      if (type==etAirp) user_fmt=reqInfo->user.sets.disp_airp;
+      if (type==etCraft) user_fmt=reqInfo->user.sets.disp_craft;
+      if (type==etSuffix) user_fmt=reqInfo->user.sets.disp_suffix;
+      if (type==etAirline ||
+          type==etAirp ||
+          type==etCraft)
+      {
+      	if ( hard_verify || fmt == -1 ) {
+          if (!(user_fmt==ustCodeNative     && fmt==0 ||
+                user_fmt==ustCodeInter      && fmt==1 ||
+                user_fmt==ustCodeICAONative && fmt==2 ||
+                user_fmt==ustCodeICAOInter  && fmt==3 ||
+                user_fmt==ustCodeMixed      && (fmt==0||fmt==1||fmt==2||fmt==3)))
+          {
+            //проблемы
+            DoElemEConvertError( ctxt, type, code );
+          }
+        }
+        else {
+          switch( user_fmt )  {
+          	case ustCodeNative:
+          		fmt = 0;
+          		break;
+          	case ustCodeInter:
+          		fmt = 1;
+          		break;
+          	case ustCodeICAONative:
+          		fmt = 2;
+          		break;
+          	case ustCodeICAOInter:
+          		fmt = 3;
+          		break;
+          	default:;
+          }
+        }
+      }
+      else
+      {
+      	if ( hard_verify || fmt == -1 ) {
+          if (!(user_fmt==ustEncNative && fmt==0 ||
+                user_fmt==ustEncLatin && fmt==1 ||
+                user_fmt==ustEncMixed && (fmt==0||fmt==1)))
+          {
+            //проблемы
+            DoElemEConvertError( ctxt, type, code );
+          }
+        }
+        else {
+          switch( user_fmt )  {
+          	case ustEncNative:
+          		fmt = 0;
+          		break;
+          	case ustEncLatin:
+          		fmt = 1;
+          		break;
+          	default:;
+          }
+
+        }
+      };
+
+    }
+    else
+    	if ( hard_verify || fmt == -1 ) {
+        if (fmt!=0)
+        {
+          //проблемы
+            DoElemEConvertError( ctxt, type, code );
+        };
+      }
+      else {
+      	fmt = 0;
+      }
+  };
+
+  return id;
+};
+
+string ElemIdToElemCtxt(TElemContext ctxt,TElemType type, string id,
+                        int fmt, bool with_deleted)
+{
+	TElemFmt fmt2=efmtCodeNative;
+  if ( ctxt==ecDisp || ctxt==ecCkin )
+  {
+    if(type==etAirline ||
+       type==etAirp ||
+       type==etCraft ||
+       type==etSuffix)
+    {
+      TReqInfo *reqInfo = TReqInfo::Instance();
+      TUserSettingType user_fmt;
+      switch (ctxt) {
+      	case ecDisp:
+          if (type==etAirline) user_fmt=reqInfo->user.sets.disp_airline;
+          if (type==etAirp) user_fmt=reqInfo->user.sets.disp_airp;
+          if (type==etCraft) user_fmt=reqInfo->user.sets.disp_craft;
+          if (type==etSuffix) user_fmt=reqInfo->user.sets.disp_suffix;
+          break;
+        case ecCkin:
+          if (type==etAirline) user_fmt=reqInfo->user.sets.ckin_airline;
+          if (type==etAirp) user_fmt=reqInfo->user.sets.ckin_airp;
+          if (type==etCraft) user_fmt=reqInfo->user.sets.ckin_craft;
+          if (type==etSuffix) user_fmt=reqInfo->user.sets.ckin_suffix;
+          break;
+        default:;
+      }
+      if (type==etAirline ||
+          type==etAirp ||
+          type==etCraft)
+      {
+        switch(user_fmt)
+        {
+          case ustCodeNative:     fmt2=efmtCodeNative; break;
+          case ustCodeInter:      fmt2=efmtCodeInter; break;
+          case ustCodeICAONative: fmt2=efmtCodeICAONative; break;
+          case ustCodeICAOInter:  fmt2=efmtCodeICAOInter; break;
+          case ustCodeMixed:      fmt2=(TElemFmt)fmt; break;
+          default: ;
+        };
+      }
+      else
+      {
+        switch(user_fmt)
+        {
+          case ustEncNative: fmt2=efmtCodeNative; break;
+          case ustEncLatin:  fmt2=efmtCodeInter; break;
+          case ustEncMixed:  fmt2=(TElemFmt)fmt; break;
+          default: ;
+        };
+      };
+    };
+  };
+
+  return ElemIdToClientElem(type,id,fmt2,with_deleted);
+};
+
+string getTableName(TElemType type)
+{
+	string table_name;
+  switch(type)
+  {
+    case etCountry:
+      table_name="countries";
+      break;
+    case etCity:
+      table_name="cities";
+      break;
+    case etAirline:
+      table_name="airlines";
+      break;
+    case etAirp:
+      table_name="airps";
+      break;
+    case etCraft:
+      table_name="crafts";
+      break;
+    case etClass:
+      table_name="classes";
+      break;
+    case etSubcls:
+      table_name="subcls";
+      break;
+    case etPersType:
+      table_name="pers_types";
+      break;
+    case etGenderType:
+      table_name="gender_types";
+      break;
+    case etPaxDocType:
+      table_name="pax_doc_types";
+      break;
+    case etPayType:
+      table_name="pay_types";
+      break;
+    case etCurrency:
+      table_name="currency";
+      break;
+    case etRefusalType:
+      table_name="refusal_types";
+      break;
+    case etTripType:
+      table_name="trip_types";
+      break;
+    case etSuffix:
+      table_name="trip_suffixes";
+      break;
+    case etCompElemType:
+      table_name="comp_elem_types";
+      break;
+    case etGrpStatusType:
+    	table_name="grp_status_types";
+    	break;
+    case etClientType:
+    	table_name="client_types";
+    	break;
+    case etCompLayerType:
+    	table_name="comp_layer_types";
+    	break;
+    case etCrs:
+    	table_name="crs2";
+    	break;
+    case etDevModel:
+    	table_name="dev_models";
+    	break;
+    case etDevSessType:
+  		table_name="dev_sess_types";
+  		break;
+    case etDevFmtType:
+  		table_name="dev_fmt_types";
+  		break;
+  	case etDevOperType:
+  		table_name="dev_oper_types";
+  		break;
+    case etGraphStage:
+  		table_name="graph_stages";
+  		break;
+  };
+  return table_name;
+}
+
+string ElemToElemId(TElemType type, const string &elem, TElemFmt &fmt, const std::string &lang, bool with_deleted)
+{
+  if (elem.empty()/* || lang.empty()*/) return "";
+
+  string id;
+  fmt=efmtUnknown;
+
+  string table_name=getTableName(type);
+
+  if (!table_name.empty())
+  {
+    //это коды
+    TBaseTable& BaseTable=base_tables.get(table_name);
+    try
+    {
+      TCodeBaseTable& CodeBaseTable=dynamic_cast<TCodeBaseTable&>(BaseTable);
+      //это code/code_lat
+      try
+      {
+        id=((TCodeBaseTableRow&)CodeBaseTable.get_row("code",elem,with_deleted)).code;
+        fmt=efmtCodeNative;
+        return id;
+      }
+      catch (EBaseTableError) {};
+      try
+      {
+        id=((TCodeBaseTableRow&)CodeBaseTable.get_row("code_lat",elem,with_deleted)).code;
+        fmt=efmtCodeInter;
+        return id;
+      }
+      catch (EBaseTableError) {};
+    }
+    catch (bad_cast) {};
+
+    try
+    {
+      TICAOBaseTable& ICAOBaseTable=dynamic_cast<TICAOBaseTable&>(BaseTable);
+      //это code_icao,code_icao_lat
+      try
+      {
+        id=((TICAOBaseTableRow&)ICAOBaseTable.get_row("code_icao",elem,with_deleted)).code;
+        fmt=efmtCodeICAONative;
+        return id;
+      }
+      catch (EBaseTableError) {};
+      try
+      {
+        id=((TICAOBaseTableRow&)ICAOBaseTable.get_row("code_icao_lat",elem,with_deleted)).code;
+        fmt=efmtCodeICAOInter;
+        return id;
+      }
+      catch (EBaseTableError) {};
+    }
+    catch (bad_cast) {};
+
+    try
+    {
+      TCountries& Countries=dynamic_cast<TCountries&>(BaseTable);
+      //это code_iso
+      try
+      {
+        id=((TCountriesRow&)Countries.get_row("code_iso",elem,with_deleted)).code;
+        fmt=efmtCodeISOInter;
+        return id;
+      }
+      catch (EBaseTableError) {};
+    }
+    catch (bad_cast) {};
+  };
+  return id;
+};
+
+string ElemToElemId(TElemType type, const string &elem, TElemFmt &fmt, bool with_deleted)
+{
+  return ElemToElemId(type, elem, fmt, "", with_deleted);
+};
+
+void getElem(TElemFmt fmt, const std::string &lang, const TBaseTableRow& BaseTableRow, string &elem)
+{
+  elem.clear();
+  switch(fmt)
+  {
+    case efmtNameShort:
+      try
+      {
+        elem=BaseTableRow.AsString("short_name", lang);
+        return;
+      }
+      catch (EBaseTableError) {};
+      break;
+
+    case efmtNameLong:
+      try
+      {
+        const TNameBaseTableRow& row=dynamic_cast<const TNameBaseTableRow&>(BaseTableRow);
+        if (lang==AstraLocale::LANG_RU)
+          elem=row.name;
+        else
+          elem=row.name_lat;
+        return;
+      }
+      catch (bad_cast) {};
+      break;
+
+    case efmtCodeNative:
+    case efmtCodeInter:
+      try
+      {
+        const TCodeBaseTableRow& row=dynamic_cast<const TCodeBaseTableRow&>(BaseTableRow);
+        if (fmt==efmtCodeNative && lang==AstraLocale::LANG_RU)
+        {
+          elem=row.code;
+          return;
+        };
+        if (fmt==efmtCodeInter ||
+            fmt==efmtCodeNative && lang!=AstraLocale::LANG_RU)
+        {
+          elem=row.code_lat;
+          return;
+        };
+      }
+      catch (bad_cast) {};
+      break;
+
+    case efmtCodeICAONative:
+    case efmtCodeICAOInter:
+      try
+      {
+        const TICAOBaseTableRow& row=dynamic_cast<const TICAOBaseTableRow&>(BaseTableRow);
+        if (fmt==efmtCodeICAONative && lang==AstraLocale::LANG_RU)
+        {
+          elem=row.code_icao;
+          return;
+        };
+        if (fmt==efmtCodeICAOInter ||
+            fmt==efmtCodeICAONative && lang!=AstraLocale::LANG_RU)
+        {
+          elem=row.code_icao_lat;
+          return;
+        };
+
+      }
+      catch (bad_cast) {};
+      break;
+
+    case efmtCodeISOInter:
+      try
+      {
+        const TCountriesRow& row=dynamic_cast<const TCountriesRow&>(BaseTableRow);
+        if (fmt==efmtCodeISOInter)
+        {
+          elem=row.code_iso;
+          return;
+        };
+      }
+      catch (bad_cast) {};
+      break;
+
+    case efmtUnknown:
+      break;
+  };
+};
+
+string ElemIdToElem(TElemType type, const string &id, const vector< pair<TElemFmt,string> > &fmts, bool with_deleted)
+{
+  if (id.empty()) return "";
+
+  string elem;
+
+  string table_name=getTableName(type);
+
+  if (!table_name.empty())
+  {
+    try
+    {
+      TBaseTableRow& BaseTableRow=base_tables.get(table_name).get_row("code",id,with_deleted);
+
+      for(vector< pair<TElemFmt,string> >::const_iterator i=fmts.begin();i!=fmts.end();i++)
+      {
+        getElem(i->first, i->second, BaseTableRow, elem);
+        if (!elem.empty()) break;
+      };
+    }
+    catch (EBaseTableError) {};
+  };
+
+  return elem;
+};
+
+string ElemIdToElem(TElemType type, const string &id, TElemFmt fmt, const std::string &lang, bool with_deleted)
+{
+  vector< pair<TElemFmt,string> > fmts;
+  fmts.push_back( make_pair(fmt, lang) );
+  return ElemIdToElem(type, id, fmts, with_deleted);
+};
+
+string ElemIdToElem(TElemType type, int id, const vector< pair<TElemFmt,string> > &fmts, bool with_deleted)
+{
+  if (id==ASTRA::NoExists) return "";
+
+  string elem;
+
+  string table_name=getTableName(type);
+
+  if (!table_name.empty())
+  {
+    try
+    {
+      TBaseTableRow& BaseTableRow=base_tables.get(table_name).get_row("id",id,with_deleted);
+
+      for(vector< pair<TElemFmt,string> >::const_iterator i=fmts.begin();i!=fmts.end();i++)
+      {
+        getElem(i->first, i->second, BaseTableRow, elem);
+        if (!elem.empty()) break;
+      };
+    }
+    catch (EBaseTableError) {};
+  };
+
+  return elem;
+};
+
+string ElemIdToElem(TElemType type, int id, TElemFmt fmt, const std::string &lang, bool with_deleted)
+{
+  vector< pair<TElemFmt,string> > fmts;
+  fmts.push_back( make_pair(fmt, lang) );
+  return ElemIdToElem(type, id, fmts, with_deleted);
+};
+
+void getElemFmts(TElemFmt fmt, vector< pair<TElemFmt,string> > &fmts)
+{
+  fmts.clear();
+  for(int pass=0; pass<=1; pass++)
+  {
+    string lang;
+    if (pass==0)
+    {
+      if (!lang.empty())
+        lang=TReqInfo::Instance()->desk.lang;
+      else
+        continue;
+    }
+    else
+    {
+      if (TReqInfo::Instance()->desk.lang!=AstraLocale::LANG_RU)
+        lang=AstraLocale::LANG_RU;
+      else
+        continue;
+    };
+
+    switch(fmt)
+    {
+       case efmtNameLong:
+         fmts.push_back( make_pair(efmtNameLong, lang) );
+       case efmtNameShort:
+         fmts.push_back( make_pair(efmtNameShort, lang) );
+         fmts.push_back( make_pair(efmtCodeNative, lang) );
+         fmts.push_back( make_pair(efmtCodeInter, lang) );
+         fmts.push_back( make_pair(efmtCodeICAONative, lang) );
+         fmts.push_back( make_pair(efmtCodeICAOInter, lang) );
+         fmts.push_back( make_pair(efmtCodeISOInter, lang) );
+         break;
+       case efmtCodeISOInter:
+         fmts.push_back( make_pair(efmtCodeISOInter, lang) );
+         fmts.push_back( make_pair(efmtCodeInter, lang) );
+         fmts.push_back( make_pair(efmtCodeNative, lang) );
+         fmts.push_back( make_pair(efmtCodeICAOInter, lang) );
+         fmts.push_back( make_pair(efmtCodeICAONative, lang) );
+         break;
+       case efmtCodeICAOInter:
+         fmts.push_back( make_pair(efmtCodeICAOInter, lang) );
+         fmts.push_back( make_pair(efmtCodeICAONative, lang) );
+         fmts.push_back( make_pair(efmtCodeInter, lang) );
+         fmts.push_back( make_pair(efmtCodeNative, lang) );
+         fmts.push_back( make_pair(efmtCodeISOInter, lang) );
+         break;
+       case efmtCodeICAONative:
+         fmts.push_back( make_pair(efmtCodeICAONative, lang) );
+         fmts.push_back( make_pair(efmtCodeICAOInter, lang) );
+         fmts.push_back( make_pair(efmtCodeNative, lang) );
+         fmts.push_back( make_pair(efmtCodeInter, lang) );
+         fmts.push_back( make_pair(efmtCodeISOInter, lang) );
+         break;
+       case efmtCodeInter:
+         fmts.push_back( make_pair(efmtCodeInter, lang) );
+         fmts.push_back( make_pair(efmtCodeNative, lang) );
+         fmts.push_back( make_pair(efmtCodeICAOInter, lang) );
+         fmts.push_back( make_pair(efmtCodeICAONative, lang) );
+         fmts.push_back( make_pair(efmtCodeISOInter, lang) );
+         break;
+       case efmtCodeNative:
+         fmts.push_back( make_pair(efmtCodeNative, lang) );
+         fmts.push_back( make_pair(efmtCodeInter, lang) );
+         fmts.push_back( make_pair(efmtCodeICAONative, lang) );
+         fmts.push_back( make_pair(efmtCodeICAOInter, lang) );
+         fmts.push_back( make_pair(efmtCodeISOInter, lang) );
+         break;
+       case efmtUnknown:
+         break;
+    };
+  };
+};
+
+string ElemIdToClientElem(TElemType type, const string &id, TElemFmt fmt, bool with_deleted)
+{
+  vector< pair<TElemFmt,string> > fmts;
+  getElemFmts(fmt, fmts);
+  return ElemIdToElem(type, id, fmts, with_deleted);
+};
+
+string ElemIdToClientElem(TElemType type, int id, TElemFmt fmt, bool with_deleted)
+{
+  vector< pair<TElemFmt,string> > fmts;
+  getElemFmts(fmt, fmts);
+  return ElemIdToElem(type, id, fmts, with_deleted);
+};
+
+string ElemIdToCodeNative(TElemType type, const string &id)
+{
+  return ElemIdToClientElem(type, id, efmtCodeNative, true);
+};
+
+string ElemIdToCodeNative(TElemType type, int id)
+{
+  return ElemIdToClientElem(type, id, efmtCodeNative, true);
+};
+
+string ElemIdToNameLong(TElemType type, const string &id)
+{
+  return ElemIdToClientElem(type, id, efmtNameLong, true);
+};
+
+string ElemIdToNameLong(TElemType type, int id)
+{
+  return ElemIdToClientElem(type, id, efmtNameLong, true);
+};
+
+string ElemIdToNameShort(TElemType type, const string &id)
+{
+  return ElemIdToClientElem(type, id, efmtNameShort, true);
+};
+
+string ElemIdToNameShort(TElemType type, int id)
+{
+  return ElemIdToClientElem(type, id, efmtNameShort, true);
+};
+
+TElemFmt prLatToElemFmt(TElemFmt fmt, bool pr_lat)
+{
+  TElemFmt res=fmt;
+  if (pr_lat)
+  {
+    if (fmt==efmtCodeNative) res=efmtCodeInter;
+    if (fmt==efmtCodeICAONative) res=efmtCodeICAOInter;
+  };
+  return res;
+};
+
---
+++ src/astra_elems.h
@@ -0,0 +1,72 @@
+#ifndef _ASTRA_ELEMS_H_
+#define _ASTRA_ELEMS_H_
+
+#include <string>
+#include <vector>
+
+enum TElemType { etCountry,etCity,etAirline,etAirp,etCraft,etClass,etSubcls,
+                 etPersType,etGenderType,etPaxDocType,etPayType,etCurrency,
+                 etRefusalType,etSuffix,etClsGrp,etTripType, etCompElemType,
+                 etGrpStatusType,etClientType,etCompLayerType,etCrs,
+                 etDevModel,etDevSessType,etDevFmtType,etDevOperType,
+                 etGraphStage };
+enum TElemContext { ecDisp, ecCkin, ecTrfer, ecTlgTypeB, ecNone };
+
+//форматы:
+enum TElemFmt {efmtUnknown=-1,
+               efmtCodeNative=0,     //вн. код IATA
+               efmtCodeInter=1,      //меж. код IATA
+               efmtCodeICAONative=2, //вн. код ICAO
+               efmtCodeICAOInter=3,  //меж. код ICAO
+               efmtCodeISOInter=4,   //меж. код ISO
+               efmtNameLong=10,      //длинное название
+               efmtNameShort=12};    //сокращенное название
+
+const char* EncodeElemContext(const TElemContext ctxt);
+const char* EncodeElemType(const TElemType type);
+
+std::string ElemToElemId(TElemType type, const std::string &elem, TElemFmt &fmt, const std::string &lang, bool with_deleted=false);
+std::string ElemToElemId(TElemType type, const std::string &elem, TElemFmt &fmt, bool with_deleted=false);
+
+
+//все нижеописанные функции дублируются для строкового и числового идентификаторов елемента
+
+//на вход подается вектор пар <формат элемента, язык>
+//поочередно перебираются элементы вектора до тех пор пока не найдется строковое представление элемента
+//если ни одно представление не найдено, возвращается пустая строка
+std::string ElemIdToElem(TElemType type, const std::string &id, const std::vector< std::pair<TElemFmt, std::string> > &fmts, bool with_deleted=true);
+std::string ElemIdToElem(TElemType type, int id, const std::vector< std::pair<TElemFmt, std::string> > &fmts, bool with_deleted=true);
+
+//если представление с параметрами формат элемента, язык не найдено, возвращается пустая строка
+std::string ElemIdToElem(TElemType type, const std::string &id, TElemFmt fmt, const std::string &lang, bool with_deleted=true);
+std::string ElemIdToElem(TElemType type, int id, TElemFmt fmt, const std::string &lang, bool with_deleted=true);
+
+//в зависимости от параметра fmt внутри функции наполняется ветор перебора (вектор пар <формат элемента, язык>)
+//учитывается язык терминала
+//если на языке терминала ничего не найдено, принимется язык LANG_RU
+//если и на языке LANG_RU ничего не найдено, возвращается пустая строка
+std::string ElemIdToClientElem(TElemType type, const std::string &id, TElemFmt fmt, bool with_deleted=true);
+std::string ElemIdToClientElem(TElemType type, int id, TElemFmt fmt, bool with_deleted=true);
+
+//вызывается ElemIdToClientElem с fmt=efmtCodeNative
+std::string ElemIdToCodeNative(TElemType type, const std::string &id);
+std::string ElemIdToCodeNative(TElemType type, int id);
+
+//вызывается ElemIdToClientElem с fmt=efmtNameLong
+std::string ElemIdToNameLong(TElemType type, const std::string &id);
+std::string ElemIdToNameLong(TElemType type, int id);
+
+//вызывается ElemIdToClientElem с fmt=efmtNameShort
+std::string ElemIdToNameShort(TElemType type, const std::string &id);
+std::string ElemIdToNameShort(TElemType type, int id);
+
+TElemFmt prLatToElemFmt(TElemFmt fmt, bool pr_lat);
+
+//перекодировки в контексте
+std::string ElemCtxtToElemId(TElemContext ctxt,TElemType type, std::string code,
+                             int &fmt, bool hard_verify, bool with_deleted=false);
+std::string ElemIdToElemCtxt(TElemContext ctxt,TElemType type, std::string id,
+                             int fmt, bool with_deleted=true);
+
+
+#endif /*_ASTRA_ELEMS_H_*/
