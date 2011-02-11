3ba732f0d8e14ee0f7f1392aa2830ed5ad7373a0
jen <jen@sirena2000.ru>
2011-02-11 10:42:14 +0000

    bug fix change craft, but prior compon is actual - return 0, we not send error to client
    git-svn-id: svn+ssh://10.1.90.223/SVNroot/astra/server/trunk@119501 e9a6705e-e829-0410-83f2-cebdff2db7e5

--- src/sopp.cc
+++ src/sopp.cc
@@ -3438,7 +3438,7 @@ void internal_WriteDests( int &move_id, TSOPPDests &dests, const string &referen
   					                                 LParams() << LParam("airp", ElemIdToCodeNative(etAirp,id->airp)));
   	}
    if ( reSetCraft ) {
-   	 if ( SALONS::AutoSetCraft( id->point_id, id->craft, -1 ) > 0 )
+   	 if ( SALONS::AutoSetCraft( id->point_id, id->craft, -1 ) >= 0 )
    	 	 ch_craft = false;
    }
   	point_num++;
