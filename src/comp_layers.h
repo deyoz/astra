4d30833ad07744a721bc1d8dd6ffa9ce0c434d73
Chicherov Vlad <vlad@sirena2000.ru>
2011-04-12 02:15:22 +0000

    Paid check-in continued...
    git-svn-id: svn+ssh://10.1.90.223/SVNroot/astra/server/trunk@121763 e9a6705e-e829-0410-83f2-cebdff2db7e5

---
+++ src/comp_layers.h
@@ -0,0 +1,50 @@
+#ifndef _COMP_LAYERS_H_
+#define _COMP_LAYERS_H_
+
+#include <string>
+#include <vector>
+#include "astra_consts.h"
+#include "seats_utils.h"
+
+//добавление слоя в tlg_comp_layers с синхронизацией trip_comp_layers и записью в журнал операций для clt...Pay, cltProtCkin
+void InsertTlgSeatRanges(int point_id_tlg,
+                         std::string airp_arv,
+                         ASTRA::TCompLayerType layer_type,
+                         const std::vector<TSeatRange> &ranges,
+                         int crs_pax_id,  //может быть NoExists
+                         int tlg_id,      //может быть NoExists
+                         int timeout,     //может быть NoExists
+                         bool UsePriorContext,
+                         int &curr_tid);  //если NoExists, то инициализируется в процедуре, служит для обновления crs_pax.tid
+//удаление слоя из tlg_comp_layers с синхронизацией trip_comp_layers и записью в журнал операций для clt...Pay, cltProtCkin
+void DeleteTlgSeatRanges(ASTRA::TCompLayerType layer_type,
+                         int crs_pax_id,           //не может быть NoExists
+                         int &curr_tid);           //если NoExists, то инициализируется в процедуре, служит для обновления crs_pax.tid
+//вызывается из описанной выше DeleteTlgSeatRanges либо из astra_timer
+void DeleteTlgSeatRanges(std::vector<int> range_ids,
+                         int crs_pax_id,           //может быть NoExists
+                         int &curr_tid);           //если NoExists, то инициализируется в процедуре, служит для обновления crs_pax.tid
+                         
+void InsertTripCompLayers(int point_id_tlg, //point_id_tlg либо point_id_spp м.б. NoExists
+                          int point_id_spp,
+                          ASTRA::TCompLayerType layer_type);
+
+void DeleteTripCompLayers(int point_id_tlg, //point_id_tlg либо point_id_spp м.б. NoExists
+                          int point_id_spp,
+                          ASTRA::TCompLayerType layer_type);
+                          
+void SyncTripCompLayers(int point_id_tlg, //point_id_tlg либо point_id_spp м.б. NoExists
+                        int point_id_spp,
+                        ASTRA::TCompLayerType layer_type);
+
+//возвращает слой если ремарка места PNL для указанной компании может быть использована для платной регистрации
+//иначе возвращает cltUnknown
+ASTRA::TCompLayerType GetSeatRemLayer(const std::string &airline_mark, const std::string &seat_rem);
+
+typedef std::vector< std::pair<std::string, int> > TSeatRemPriority;
+//возвращает сортированный! по приоритетам список ремарок
+void GetSeatRemPriority(const std::string &airline_mark, TSeatRemPriority &rems);
+
+
+#endif
+
