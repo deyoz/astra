50628c8a82a6061de04cac1ec269fb552be6d560
Vlad Chicherov <vlad@sirena2000.ru>
2013-06-25 12:53:41 +0000

    Merge from TRUNK -c148987. Web search bug fixed
    git-svn-id: svn+ssh://10.1.90.223/SVNroot/astra/server/stand@148988 e9a6705e-e829-0410-83f2-cebdff2db7e5

--- src/web_main.cc
+++ src/web_main.cc
@@ -241,7 +241,16 @@ void WebRequestsIface::SearchFlt(XMLRequestCtxt *ctxt, xmlNodePtr reqNode, xmlNo
   if (scanCodeNode!=NULL)
     filter.fromBCBP_M(NodeAsString(scanCodeNode));
   else
+  {
     filter.fromXML(reqNode);
+    if (filter.document.empty() &&
+        filter.ticket_no.empty() &&
+        filter.pnr_addr_normal.empty())
+    {
+      ProgError(STDLOG, "WebRequestsIface::SearchFlt: <pnr_addr>, <ticket_no>, <document> not defined");
+      throw UserException("MSG.NOTSET.SEARCH_PARAMS");
+    };
+  };
   filter.testPaxFromDB();
   filter.trace(TRACE5);
   
