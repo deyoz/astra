a7bbfaca8e27d8161ae1b0dfc3eb32d553cfe4dd
Chicherov Vlad <vlad@sirena2000.ru>
2013-10-17 10:56:17 +0000

    EDI_US iteration
    git-svn-id: svn+ssh://10.1.90.223/SVNroot/astra/server/trunk@152497 e9a6705e-e829-0410-83f2-cebdff2db7e5

--- src/apis.h
+++ src/apis.h
@@ -1,6 +1,22 @@
 #ifndef _APIS_H_
 #define _APIS_H_
 
+#include <set>
+#include <string>
+
+class TCountriesUS : public std::set<std::string>
+{
+  public:
+    TCountriesUS()
+    {
+      insert("ƒ“");
+      insert("Œ");
+      insert("‘");
+    };
+};
+
+static TCountriesUS countriesUS;
+
 void create_apis_file(int point_id);
 void create_apis_nosir_help(const char *name);
 int create_apis_nosir(int argc,char **argv);
