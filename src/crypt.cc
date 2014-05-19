1cd5aa7eecc044c9cda47e1f0db4744683ec45bc
Chicherov Vlad <vlad@sirena2000.ru>
2014-05-19 07:23:44 +0000

    #tech astra specific crypting error handler
    git-svn-id: svn+ssh://10.1.90.223/SVNroot/astra/server/trunk@159621 e9a6705e-e829-0410-83f2-cebdff2db7e5

--- src/crypt.cc
+++ src/crypt.cc
@@ -83,8 +83,8 @@ size_t form_crypt_error(char* res, size_t res_len, const char* head, size_t hlen
   }
 
   ProgTrace(TRACE1,"Crypting error occured: (%i) '%s'",error,msg_error.c_str());
-  (head[byte])|=MSG_SYS_ERROR;
   memcpy(res, head, hlen);
+  res[byte] |= MSG_SYS_ERROR;
   utf8len=CP866toUTF8((unsigned char **)&utf8txt,(unsigned char *)msg_error.c_str());
   if(utf8len<=0)
     utf8txt=(char *)"System error!";
