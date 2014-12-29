ee2224ab38a60c29806f635ae398666bb4d02ba3
Anna Smirnova <a.smirnova@sirena2000.ru>
2014-12-29 16:03:07 +0000

    #19066 Checking of mandatory fields in the document added
    git-svn-id: svn+ssh://10.1.90.223/SVNroot/astra/server/trunk@165969 e9a6705e-e829-0410-83f2-cebdff2db7e5

--- src/apis_utils.h
+++ src/apis_utils.h
@@ -179,10 +179,14 @@ const long int DOC_MINTRANS_FIELDS=DOC_TYPE_FIELD|
 const long int TKN_MINTRANS_FIELDS=TKN_TICKET_NO_FIELD;
 
 //==============================================================================
-
-//==============================================================================
 const long int DOC_XML_TR_FIELDS=DOC_TYPE_FIELD|
-                                   DOC_NO_FIELD;
+                                 DOC_ISSUE_COUNTRY_FIELD|
+                                 DOC_NO_FIELD|
+                                 DOC_NATIONALITY_FIELD|
+                                 DOC_BIRTH_DATE_FIELD|
+                                 DOC_GENDER_FIELD|
+                                 DOC_SURNAME_FIELD|
+                                 DOC_FIRST_NAME_FIELD;
 
 //==============================================================================
 
