9b2091f66c9eb4c3d329452ba9e5577729a01e25
Anton Bokov <a.bokov@sirena2000.ru>
2015-01-26 13:52:23 +0000

    #18807 - fix
    git-svn-id: svn+ssh://10.1.90.223/SVNroot/astra/server/trunk@166386 e9a6705e-e829-0410-83f2-cebdff2db7e5

--- bin/config_astra.sh
+++ bin/config_astra.sh
@@ -1,6 +1,6 @@
 if [ -r $1/env_file ] ; then 
 . $1/env_file
-(cd src && ./Config -f)
+(./Config -f)
 for i in 1 2 3 4 5 6 ; do echo ; done
 echo now you can run make or MAKE in src    
 for i in 1 2 3 4 5 6 ; do echo ; done
