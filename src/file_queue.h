fb03b704ea9de466340e00f686cd01344de7df02
Denis Zaharaov <denis_zaharov@sirena2000.ru>
2015-01-26 14:52:27 +0000

    nosir file_by_id added
    git-svn-id: svn+ssh://10.1.90.223/SVNroot/astra/server/trunk@166391 e9a6705e-e829-0410-83f2-cebdff2db7e5

--- src/file_queue.h
+++ src/file_queue.h
@@ -150,4 +150,6 @@ class TFileQueue: public std::vector<TQueueItem> {
     }
 };
 
+int file_by_id(int argc,char **argv);
+
 #endif /*_FILE_QUEUE_H_*/
--- src/obrnosir.cc
+++ src/obrnosir.cc
@@ -77,7 +77,8 @@ const
     {"-mobile_stat",            mobile_stat,            NULL,                       NULL},
     {"-test_astra_locale_adv",  test_astra_locale_adv,  NULL,                       NULL},
     {"-insert_locales",         insert_locales,         NULL,                       NULL},
-    {"-dst_points",             points_dst_format,         NULL,                       NULL}
+    {"-file_by_id",             file_by_id,             NULL,                       NULL},
+    {"-dst_points",             points_dst_format,      NULL,                       NULL}
   };
 
 int nosir_test(int argc,char **argv)
