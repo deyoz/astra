1d579b63f5d36c2920d028ddeac28ac1bb857702
Denis Zaharaov <denis_zaharov@sirena2000.ru>
2015-03-02 15:45:43 +0000

    redmine  #19298: LCI over HTTP: remove typeb_help.originator_id from existence along with all related stuff
    git-svn-id: svn+ssh://10.1.90.223/SVNroot/astra/server/trunk@167363 e9a6705e-e829-0410-83f2-cebdff2db7e5

--- src/TypeBHelpMng.h
+++ src/TypeBHelpMng.h
@@ -2,9 +2,8 @@
 #define _TYPEBHELPMNG_H
 
 namespace  TypeBHelpMng {
-    void configForPerespros(int tlgs_id, int originator_id);
+    void configForPerespros(int tlgs_id);
     bool notify(int typeb_in_id, int typeb_out_id);
-    int getOriginatorId(int typeb_in_id);
     void clean_typeb_help();
 };
 
