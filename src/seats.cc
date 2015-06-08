c27daaf527e357aa98e31bf635ac31062af7e930
jen <jen@sirena2000.ru>
2015-06-08 10:11:43 +0000

    Bug #21115 web pay seats bug fix
    git-svn-id: svn+ssh://10.1.90.223/SVNroot/astra/server/trunk@170164 e9a6705e-e829-0410-83f2-cebdff2db7e5

--- src/seats.cc
+++ src/seats.cc
@@ -241,7 +241,8 @@ string SUBCLS_REM;
 bool canUseOneRow=true; // использовать при рассадке только один ряд - определяется на основе ф-ции getCanUseOneRow() и цикла
 
 struct TCondRate {
-  bool pr_web;
+  bool pr_web; //признак того, что регистрируется платный пассажир
+  bool use_rate; // использовать платные места для пассажиров без оплаченных мест
   map<int, TPlaceWebTariff> rates;
   map<int, TPlaceWebTariff>::iterator current_rate;
   TCondRate( ) {
@@ -250,8 +251,10 @@ struct TCondRate {
   double convertUniValue( TPlaceWebTariff &rate ) {
     return rate.value;
   }
-  void Init( SALONS2::TSalons &Salons, bool apr_web ) {
-    pr_web = apr_web;
+  void Init( SALONS2::TSalons &Salons, bool apr_pay, TClientType client_type ) {
+    pr_web = apr_pay;
+    use_rate = ( client_type == ctTerm || client_type == ctPNL );
+    ProgTrace( TRACE5, "use_rate=%d", use_rate);
     rates.clear();
     rates[ 0 ].value = 0.0; // всегда задаем - означает, что надо использовать места без тарифа
     if ( pr_web ) // не учитываем платные места
@@ -293,6 +296,9 @@ struct TCondRate {
   bool CanUseRate( TPlace *place ) { /* если все возможные тарифы попробовали при рассадке и не смогли рассадить или нет тарифов на рейсе или место без тарифа, то можно использовать */
     bool res = ( pr_web || current_rate == rates.end() || place->WebTariff.value == 0.0 );
     if ( !res ) {
+      if ( !use_rate ) {
+        return res;
+      }
       for ( map<int, TPlaceWebTariff>::iterator i=rates.begin(); ; i++ ) { // просмотр всех тарифов, кот. сортированы в порядке возрастания приоритета использования
         if ( i->second.value == place->WebTariff.value ) {
           res = true;
@@ -308,7 +314,7 @@ struct TCondRate {
     map<int, TPlaceWebTariff>::iterator i = rates.end();
     if ( !rates.empty() )
       i--;
-    return ( current_rate == rates.end() || i == current_rate );
+    return ( (current_rate == rates.end() && use_rate) || i == current_rate );
   }
 };
 
@@ -1057,7 +1063,7 @@ bool TSeatPlaces::SeatSubGrp_On( SALONS2::TPoint FP, TSeatStep Step, int Wanted 
     /* далее поиск налево через проход */
     if ( foundCount < MAXPLACE() ) {
       EP.x = FP.x - foundBefore - 2; // устанавливаемся на предполагаемое место
-      SALONS2::TPoint VP = EP;
+      //SALONS2::TPoint VP = EP;
 //      ProgTrace( TRACE5, "EP=(%d,%d)", EP.x, EP.y );
       if ( placeList->ValidPlace( EP ) ) {
         SALONS2::TPoint p( EP.x + 1, EP.y );
@@ -1556,7 +1562,7 @@ bool TSeatPlaces::SeatsPassenger_OnBasePlace( string &placeName, TSeatStep Step 
 {
 //  ProgTrace( TRACE5, "SeatsPassenger_OnBasePlace( )" );
   bool OldCanUseSmoke = CanUseSmoke;
-  bool CanUseSmoke = false;
+  //bool CanUseSmoke = false;
   try {
     if ( !placeName.empty() ) {
       /* конвертация номеров мест пассажиров в зависимости от лат. или рус. салона */
@@ -2791,7 +2797,7 @@ void SeatsPassengers( SALONS2::TSalons *Salons,
     else
         pr_all_pass_SUBCLS = false;
   }
-  condRates.Init( *Salons, pr_pay ); // собирает все типы платных мест в массив по приоритетам, если это web-клиент, то не учитываем
+  condRates.Init( *Salons, pr_pay, TReqInfo::Instance()->client_type ); // собирает все типы платных мест в массив по приоритетам, если это web-клиент, то не учитываем
 
   ProgTrace( TRACE5, "pr_SUBCLS=%d,pr_all_pass_SUBCLS=%d,SUBCLS_REM=%s,pr_pay=%d,prINFT=%d",
              pr_SUBCLS, pr_all_pass_SUBCLS, SUBCLS_REM.c_str(), pr_pay, prINFT );
