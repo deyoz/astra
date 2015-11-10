3b4901227f8e60a418460274d87cb3a279f760e5
Anna Smirnova <a.smirnova@sirena2000.ru>
2015-11-10 03:13:12 +0000

    #23017 CSV_AE apis format added
    git-svn-id: svn+ssh://10.1.90.223/SVNroot/astra/server/trunk@175038 e9a6705e-e829-0410-83f2-cebdff2db7e5

--- src/apis_tools.cc
+++ src/apis_tools.cc
@@ -4,6 +4,8 @@
 #include "base_tables.h"
 #include "exceptions.h"
 #include "astra_utils.h"
+#include "astra_misc.h"
+#include "basic.h"
 
 #include <boost/uuid/uuid.hpp>
 #include <boost/uuid/uuid_generators.hpp>
@@ -114,4 +116,43 @@ void set_trip_apis_param(const int point_id, const std::string& format, const st
   {
     if (E.Code!=1) throw;
   };
+}
+
+void getTBTripItem(const int point_dep, const int point_arv, const std::string& country,
+                   std::string& tb_date, std::string& tb_time, std::string& tb_airp )
+
+{
+  TAdvTripRoute route, tmp;
+  route.GetRouteBefore( ASTRA::NoExists, point_dep, trtWithCurrent, trtNotCancelled );
+  tmp.GetRouteAfter( ASTRA::NoExists, point_dep, trtNotCurrent, trtNotCancelled );
+  route.insert( route.end(), tmp.begin(), tmp.end() );
+
+  std::string airp;
+  BASIC::TDateTime date_time = ASTRA::NoExists;
+  if ( getCountryByAirp(route.front().airp).code_lat != country ) {
+    // inbound flight
+    for(TAdvTripRoute::const_iterator it=route.begin(); it->point_id!=point_arv; it++) {
+      if (getCountryByAirp(it->airp).code_lat == country) {
+        airp = it->airp;
+        date_time = it->scd_in;
+      }
+    }
+  }
+  else {
+    for(TAdvTripRoute::const_reverse_iterator rit=route.rbegin(); rit->point_id!=point_dep; rit++) {
+      // outbound flight
+      if (getCountryByAirp(rit->airp).code_lat == country) {
+        airp = rit->airp;
+        date_time = rit->scd_out;
+      };
+    }
+  }
+
+  if ( !airp.empty() && date_time!=ASTRA::NoExists ) {
+    TAirpsRow airp_row = (TAirpsRow&)base_tables.get("airps").get_row("code",airp);
+    BASIC::TDateTime date_time_local = UTCToLocal(date_time,AirpTZRegion(airp_row.code));
+    tb_date = BASIC::DateTimeToStr(date_time_local,"dd.mm.yyyy");
+    tb_time = BASIC::DateTimeToStr(date_time_local,"hh:nn");
+    tb_airp = airp_row.code_lat;
+  }
 }
