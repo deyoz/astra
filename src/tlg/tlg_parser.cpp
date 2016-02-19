85ed7699ce54bf2666cdf6dc12d32838d4d25620
Vlad Chicherov <vlad@sirena2000.ru>
2016-02-19 11:23:44 +0000

    #24155 #23143. Merge from STAND -c178199. DEL bug fixed in PNL/ADL parser
    git-svn-id: svn+ssh://10.1.90.223/SVNroot/astra/server/stable@178200 e9a6705e-e829-0410-83f2-cebdff2db7e5

--- src/tlg/tlg_parser.cpp
+++ src/tlg/tlg_parser.cpp
@@ -3143,7 +3143,7 @@ void BindRemarks(TTlgParser &tlg, TNameElement &ne)
   int res,k;
   string::size_type pos;
   bool pr_parse;
-  string strh;  
+  string strh;
   char rem_code[7],numh[4];
   int num;
   vector<TRemItem>::iterator iRemItem;
@@ -6759,9 +6759,11 @@ bool SavePNLADLPRLContent(int tlg_id, TDCSHeadingInfo& info, TPNLADLPRLContent& 
               PnrAddrsQry.Execute();
             };
 
+            bool onlyDEL=true;
             for(iNameElement=pnr.ne.begin();iNameElement!=pnr.ne.end();iNameElement++)
             {
               TNameElement& ne=*iNameElement;
+              if (ne.indicator!=DEL) onlyDEL=false;
               CrsPaxQry.SetVariable("surname",ne.surname);
               CrsPaxQry.SetVariable("seats",1);
               for(iPaxItem=ne.pax.begin();iPaxItem!=ne.pax.end();iPaxItem++)
@@ -6954,8 +6956,7 @@ bool SavePNLADLPRLContent(int tlg_id, TDCSHeadingInfo& info, TPNLADLPRLContent& 
             }; //for(iNameElement=pnr.ne.begin()
 
             //запишем стыковки
-            //if (/*ne.indicator==ADD && */!pnr.transfer.empty() /*||
-            //    ne.indicator==CHG*/)
+            if (!onlyDEL)
             {
               //удаляем нафиг все стыковки
               Qry.Clear();
@@ -6989,8 +6990,7 @@ bool SavePNLADLPRLContent(int tlg_id, TDCSHeadingInfo& info, TPNLADLPRLContent& 
               Qry.Execute();
             };
             //запишем коммерческий рейс
-            //if (/*ne.indicator==ADD && */!pnr.market_flt.Empty() /*||
-            //    ne.indicator==CHG*/)
+            if (!onlyDEL)
             {
               //удаляем нафиг предыдущий
               Qry.Clear();
