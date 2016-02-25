3f52304af5ee20e60ebe2f962e2a1ae28f12b18b
Anton Bokov <a.bokov@sirena2000.ru>
2016-02-25 14:27:18 +0000

    #20090
    git-svn-id: svn+ssh://10.1.90.223/SVNroot/astra/server/trunk@178314 e9a6705e-e829-0410-83f2-cebdff2db7e5

--- src/run.sh
+++ src/run.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
-
+if [ -f ../locallibs/external_env_file ] ; then . ../locallibs/external_env_file ; fi
 if [ $# -gt 0 ]
 then
     LC_ALL=C \
-    LD_LIBRARY_PATH=.:libs:airimp:${ORACLE_HOME}/lib:${LD_LIBRARY_PATH} \
+    LD_LIBRARY_PATH=.:libs:airimp:${ORACLE_HOME}/lib:${BOOST}/lib:${LD_LIBRARY_PATH} \
     exec ${1+"$@"}
 fi
