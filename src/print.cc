618205e5011d44535a824a5282d8f3601e038353
Denis Zaharaov <denis_zaharov@sirena2000.ru>
2016-03-23 09:12:59 +0000

    redmine #22450: KIOSK Graphics2D: b64_encode for Graphics2D
    git-svn-id: svn+ssh://10.1.90.223/SVNroot/astra/server/trunk@178948 e9a6705e-e829-0410-83f2-cebdff2db7e5

--- src/print.cc
+++ src/print.cc
@@ -1703,6 +1703,8 @@ void PrintInterface::GetPrintDataBP(const BPParams &params,
             StringToHex( string(iPax->prn_form), iPax->prn_form );
             iPax->hex=true;
         }
+        if(DecodeDevFmtType(params.fmt_type) == dftGraphics2D)
+            iPax->prn_form = StrUtils::b64_encode(iPax->prn_form);
         if(iPax->pax_id!=NoExists)
             parser->pts.save_bp_print();
         iPax->time_print=parser->pts.get_time_print();
