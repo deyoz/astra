e354b2119d7ccc2321d3e3d5b3033f1d13e64adb
Denis Zaharaov <denis_zaharov@sirena2000.ru>
2016-06-03 12:41:29 +0000

    #24216: stat orders: fix STAT_ORDERS_VERSION
    git-svn-id: svn+ssh://10.1.90.223/SVNroot/astra/server/trunk@181258 e9a6705e-e829-0410-83f2-cebdff2db7e5

--- src/term_version.h
+++ src/term_version.h
@@ -43,13 +43,13 @@
 #define MUSE_DEV_VARIABLES            "201503-0167752"
 #define PIECE_CONCEPT_VERSION         "201509-0172727"
 #define PIECE_CONCEPT_VERSION2        "201509-0173253"
+#define STAT_ORDERS_VERSION           "201605-0180719"
 #define FQT_TIER_LEVEL_VERSION        "201606-0000000"  //!!!vlad
 #define RFISC_VERSION                 "201605-0181021" //cute certificate
 
 #define TRANSIT_CRAFT_VERSION         "201700-0000000"
 #define EMDA_VERSION                  "202000-0000000"
 #define DOCO_CONFIRM_VERSION             "202000-0000000"
-#define STAT_ORDERS_VERSION           "202000-0000000"
 
 #endif
 
