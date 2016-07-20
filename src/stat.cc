439f3a58da7732acce5b83b583b8838f3b597056
Denis Zaharaov <denis_zaharov@sirena2000.ru>
2016-07-20 12:49:21 +0000

    merge -c182872 from trunk: #25751: unaccomp_bag_info stat: online stat limited by month
    git-svn-id: svn+ssh://10.1.90.223/SVNroot/astra/server/stand@182873 e9a6705e-e829-0410-83f2-cebdff2db7e5

--- src/stat.cc
+++ src/stat.cc
@@ -8282,7 +8282,8 @@ void StatInterface::RunStat(XMLRequestCtxt *ctxt, xmlNodePtr reqNode, xmlNodePtr
             params.statType==statTlgOutShort ||
             params.statType==statTlgOutDetail ||
             params.statType==statTlgOutFull ||
-            params.statType==statRFISC
+            params.statType==statRFISC ||
+            params.statType==statUnaccBag
        )
     {
         if(IncMonth(params.FirstDate, 1) < params.LastDate)
