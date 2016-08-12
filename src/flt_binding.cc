c96ecefe2d43fef75a4732ab9c4ce7b9e44ce0a2
Denis Zaharaov <denis_zaharov@sirena2000.ru>
2016-08-12 11:03:49 +0000

    #25962; #25963: receive CPM, UCM, SLS
    git-svn-id: svn+ssh://10.1.90.223/SVNroot/astra/server/trunk@183932 e9a6705e-e829-0410-83f2-cebdff2db7e5

--- src/flt_binding.cc
+++ src/flt_binding.cc
@@ -9,7 +9,7 @@
 
 #define STDLOG NICKNAME,__FILE__,__LINE__
 #define NICKNAME "VLAD"
-#include "serverlib/test.h"
+#include "serverlib/slogger.h"
 
 using namespace ASTRA;
 using namespace BASIC::date_time;
@@ -598,5 +598,18 @@ void TFltBinding::trace_for_bind(const vector<int> &point_ids, const string &whe
     trace << " " << *id << ";";
 
   ProgTrace(TRACE5, "%s", trace.str().c_str());
+}
+
+void TFltInfo::dump()
+{
+    LogTrace(TRACE5) << "----TFltInfo::dump----";
+    LogTrace(TRACE5) << "airline: " << airline;
+    LogTrace(TRACE5) << "flt_no: " << flt_no;
+    LogTrace(TRACE5) << "suffix: " << suffix;
+    LogTrace(TRACE5) << "scd: " << DateTimeToStr(scd);
+    LogTrace(TRACE5) << "pr_utc: " << pr_utc;
+    LogTrace(TRACE5) << "airp_dep: " << airp_dep;
+    LogTrace(TRACE5) << "airp_arv: " << airp_arv;
+    LogTrace(TRACE5) << "----------------------";
 }
 
--- src/tlg/typeb_handler.cpp
+++ src/tlg/typeb_handler.cpp
@@ -8,6 +8,7 @@
 #include "tlg.h"
 #include "tlg_parser.h"
 #include "lci_parser.h"
+#include "ucm_parser.h"
 #include "mvt_parser.h"
 #include "typeb_utils.h"
 #include "telegram.h"
@@ -996,6 +997,17 @@ bool parse_tlg(void)
             count++;
             break;
           }
+          case tcUCM:
+          case tcCPM:
+          case tcSLS:
+          {
+              TUCMHeadingInfo &info = *(dynamic_cast<TUCMHeadingInfo*>(HeadingInfo));
+              SaveFlt(tlg_id,info.flt_info.toFltInfo(),btFirstSeg);
+              parseTypeB(tlg_id);
+              ASTRA::commit();//OraSession.Commit();
+              count++;
+              break;
+          }
           case tcLCI:
           {
             TLCIHeadingInfo &info = *(dynamic_cast<TLCIHeadingInfo*>(HeadingInfo));
@@ -1112,6 +1124,7 @@ void get_tlg_info(
         part.offset = parts.addr.size();
         ParseHeading(part, HeadingInfo, bind_flts, mem);
 
+        LogTrace(TRACE5) << "tlg_type: " << tlg_type;
         tlg_type = HeadingInfo->tlg_type;
 
         part.p=parts.body.c_str();
@@ -1126,6 +1139,15 @@ void get_tlg_info(
                 {
                     TLCIHeadingInfo &info = *(dynamic_cast<TLCIHeadingInfo*>(HeadingInfo));
                     airline = info.flt_info.flt.airline.c_str();
+                    airp = info.flt_info.airp;
+                    break;
+                }
+            case tcUCM:
+            case tcCPM:
+            case tcSLS:
+                {
+                    TUCMHeadingInfo &info = *(dynamic_cast<TUCMHeadingInfo*>(HeadingInfo));
+                    airline = info.flt_info.airline;
                     airp = info.flt_info.airp;
                     break;
                 }
