11a779d8c3a3eaa637c66eaba8492f8d6f7905f6
Chicherov Vlad <vlad@sirena2000.ru>
2016-09-14 14:50:43 +0000

    #26611. <bag_norm> added into <LoadPnr> and <SavePax> web-answers
    git-svn-id: svn+ssh://10.1.90.223/SVNroot/astra/server/trunk@185105 e9a6705e-e829-0410-83f2-cebdff2db7e5

--- src/web_main.h
+++ src/web_main.h
@@ -7,6 +7,7 @@
 #include "typeb_utils.h"
 #include "web_search.h"
 #include "checkin_utils.h"
+#include "etick.h"
 #include <tuple>
 #include <vector>
 #include <cstdint>
@@ -135,6 +136,7 @@ struct TWebPax {
     CheckIn::TPaxTknItem tkn;
     CheckIn::TPaxDocItem doc;
     CheckIn::TPaxDocoItem doco;
+    TETickItem etick;
     std::list<CheckIn::TPaxDocaItem> doca;
     std::set<CheckIn::TPaxFQTItem> fqts;
     std::multiset<CheckIn::TPaxRemItem> rems;
