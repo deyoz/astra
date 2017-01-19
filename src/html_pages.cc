1e2e614217a6fc5d1376a361f06038815c69f1e2
Grigory Koshkin <g.koshkin@sirena2000.ru>
2017-01-19 11:57:11 +0000

    html pages db
    git-svn-id: svn+ssh://10.1.90.223/SVNroot/astra/server/trunk@189752 e9a6705e-e829-0410-83f2-cebdff2db7e5

---
+++ src/html_pages.cc
@@ -0,0 +1,136 @@
+#include "html_pages.h"
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include "boost/filesystem/operations.hpp"
+#include "qrys.h"
+#include "exceptions.h"
+#include "astra_context.h"
+#include "serverlib/str_utils.h"
+
+using namespace std;
+using namespace EXCEPTIONS;
+namespace fs = boost::filesystem;
+
+const bool LOCAL_DEBUG = true;
+
+void html_db_usage(string name, string what)
+{
+    cout    << "Error: " << what << endl
+            << "Usage:" << endl
+            << " " << name << " <html_dir>" << endl
+            << "Example:" << endl
+            << " " << name << " html" << endl;
+}
+
+vector<string> get_file_list(const string& init_path)
+{
+    //  https://gist.github.com/vivithemage/9517678
+    //  http://stackoverflow.com/questions/67273/how-do-you-iterate-through-every-file-directory-recursively-in-standard-c
+    vector<string> file_list;
+    if (init_path.empty())
+        throw Exception("Path is empty");
+    fs::recursive_directory_iterator end;
+    for (fs::recursive_directory_iterator i_entry(init_path); i_entry != end; ++i_entry)
+        if (fs::is_regular_file(i_entry->path()))
+            file_list.push_back( i_entry->path().string().substr(init_path.size()) );
+    return file_list;
+}
+
+string file_to_string(const string& full_path)
+{
+    //  http://www.cplusplus.com/reference/istream/istream/tellg/
+    //  http://stackoverflow.com/questions/9518659/how-to-read-a-specific-amount-of-characters-from-a-text-file
+    if (LOCAL_DEBUG) cout << __FUNCTION__ << " READ FILE: " << full_path << endl;
+    ifstream in(full_path.c_str(), std::ios::binary);
+    if (!in.good())
+        throw Exception("Cannot open for read file %s", full_path.c_str());
+    return StrUtils::b64_encode(string( std::istreambuf_iterator<char>(in), std::istreambuf_iterator<char>() ));
+}
+
+void file_to_db(const string& init_path, const string& file_path)
+{
+    TCachedQuery Qry1(
+        "begin "
+        "   delete from HTML_PAGES_TEXT where id = (select id from HTML_PAGES where name = :name); "
+        "   delete from HTML_PAGES where name = :name; "
+        "   insert into HTML_PAGES values(tid__seq.nextval, :name) returning id into :id; "
+        "end; ",
+        QParams()
+        << QParam("name", otString, file_path)
+        << QParam("id", otInteger)
+    );
+    Qry1.get().Execute();
+    int id = Qry1.get().GetVariableAsInteger("id");
+    TCachedQuery Qry2(
+        "insert into HTML_PAGES_TEXT(id, page_no, text) values(:id, :page_no, :text)",
+        QParams()
+        << QParam("id", otInteger, id)
+        << QParam("page_no", otInteger)
+        << QParam("text", otString)
+    );
+    longToDB(Qry2.get(), "text", file_to_string(init_path + file_path));
+}
+
+//  --------------------------------
+int html_to_db(int argc, char **argv)
+{
+    try
+    {
+        if (argc != 2)
+            throw Exception("Must provide path");
+        string init_path(argv[1]);
+        vector<string> file_list = get_file_list(init_path);
+        for (vector<string>::iterator i_file_path = file_list.begin(); i_file_path != file_list.end(); ++i_file_path)
+            file_to_db(init_path, *i_file_path);
+    }
+    catch(Exception &E)
+    {
+        html_db_usage(argv[0], E.what());
+        return 1;
+    }
+    return 0;
+}
+
+//  --------------------------------
+int html_from_db(int argc, char **argv)
+{
+    try
+    {
+        if (argc != 2)
+            throw Exception("Must provide path");
+        string init_path(argv[1]);
+        TCachedQuery Qry1(
+            "select name, text from HTML_PAGES, HTML_PAGES_TEXT "
+            "where "
+            "   HTML_PAGES.id = HTML_PAGES_TEXT.id "
+            "order by "
+            "   name, page_no"
+        );
+        Qry1.get().Execute();
+        map<string, string> pages;
+        for (; not Qry1.get().Eof; Qry1.get().Next())
+        {
+            string &text = pages[Qry1.get().FieldAsString("name")];
+            text += Qry1.get().FieldAsString("text");
+        }
+        for (map<string, string>::iterator ip = pages.begin(); ip != pages.end(); ip++)
+        {
+            string file_path =  ip->first;
+            string full_path = init_path + file_path;
+            string dir_path = full_path.substr( 0, full_path.size() - fs::path(full_path).filename().string().size() );
+            fs::create_directories(dir_path);
+            if (LOCAL_DEBUG) cout << __FUNCTION__ << " WRITE FILE: " << full_path << endl;
+            ofstream out(full_path, std::ios::binary|std::ios::trunc);
+            if(!out.good())
+                throw Exception("Cannot open for write file %s", full_path.c_str());
+            out << StrUtils::b64_decode(ip->second);
+        }
+    }
+    catch(Exception &E)
+    {
+        html_db_usage(argv[0], E.what());
+        return 1;
+    }
+    return 0;
+}
---
+++ src/html_pages.h
@@ -0,0 +1,7 @@
+#ifndef _HTML_PAGES_H_
+#define _HTML_PAGES_H_
+
+int html_to_db(int argc,char **argv);
+int html_from_db(int argc,char **argv);
+
+#endif
