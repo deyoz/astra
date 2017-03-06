6558855ea0343e512ffc23d362a04e3d61e51323
Anton Bokov <a.bokov@sirena2000.ru>
2017-03-06 17:39:28 +0000

    #28254 - handle emd coupon quantity
    git-svn-id: svn+ssh://10.1.90.223/SVNroot/astra/server/trunk@191406 e9a6705e-e829-0410-83f2-cebdff2db7e5

--- src/tlg/read_edi_elements.cpp
+++ src/tlg/read_edi_elements.cpp
@@ -129,7 +129,10 @@ boost::optional<PtsElem> readEdiPts(_EDI_REAL_MES_STRUCT_ *pMes)
 
     PtsElem res;
 
-    res.m_itemNumber = GetDBNumCast<int>(EdiCast::EdiDigitCast<int>("EtsErr::EDI_PROC_ERR", "-1"), pMes, 7140);
+    int itemNum = GetDBNumCast<int>(EdiCast::EdiDigitCast<int>("EtsErr::EDI_PROC_ERR", "-1"), pMes, 7140);
+    if(itemNum >= 0 ) {
+        res.m_itemNumber = itemNum;
+    }
     res.m_fareBasis  = GetDBFName(pMes, DataElement(5242), CompElement("C643"));
     res.m_rfic       = GetDBNum  (pMes, DataElement(4183, 0, 0));
     res.m_rfisc      = GetDBNum  (pMes, DataElement(4183, 0, 1));
