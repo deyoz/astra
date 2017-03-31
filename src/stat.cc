c8e377c419c5dc8246312dc34241c3d23083c18a
Denis Zaharaov <denis_zaharov@sirena2000.ru>
2017-03-31 09:29:02 +0000

    merge -c192309  from trunk; #27257: 1Q17 plan, 1.20: PFS stat
    git-svn-id: svn+ssh://10.1.90.223/SVNroot/astra/server/stand@192333 e9a6705e-e829-0410-83f2-cebdff2db7e5

--- src/stat.cc
+++ src/stat.cc
@@ -1904,6 +1904,8 @@ enum TStatType {
     statLimitedCapab,
     statUnaccBag,
     statAnnulBT,
+    statPFSShort,
+    statPFSFull,
     statNum
 };
 
@@ -1926,7 +1928,9 @@ const char *TStatTypeS[statNum] = {
     "statService",
     "statLimitedCapab",
     "statUnaccBag",
-    "statAnnulBT"
+    "statAnnulBT",
+    "statPFSShort",
+    "statPFSFull"
 };
 
 TStatType DecodeStatType( const string stat_type )
@@ -2405,6 +2409,13 @@ void TStatParams::get(xmlNodePtr reqNode)
     } else if(type == "Аннул. бирки") {
         if(name == "Подробная")
             statType=statAnnulBT;
+        else
+            throw Exception("Unknown stat mode " + name);
+    } else if(type == "PFS") {
+        if(name == "Подробная")
+            statType = statPFSFull;
+        else if(name == "Общая")
+            statType = statPFSShort;
         else
             throw Exception("Unknown stat mode " + name);
     } else
@@ -6228,7 +6239,9 @@ void RunRFISCStat(
         else
           SQLText +=
             "    points.scd_out >= :FirstDate AND points.scd_out < :LastDate \n";
+
         TCachedQuery Qry(SQLText, QryParams);
+
         Qry.get().Execute();
         if(not Qry.get().Eof) {
             int col_rfisc = Qry.get().GetFieldIndex("rfisc");
@@ -9334,6 +9347,399 @@ void orderStat(const TStatParams &params, XMLRequestCtxt *ctxt, xmlNodePtr reqNo
     }
 }
 
+/*------------------------------- PFS STAT ---------------------------------------*/
+struct TPFSStatRow {
+    int point_id;
+    int pax_id;
+    TDateTime scd_out;
+    string flt;
+    string status;
+    string route;
+    int seats;
+    string subcls;
+    string pnr;
+    string surname;
+    string name;
+    string gender;
+    TDateTime birth_date;
+    TPFSStatRow():
+        point_id(NoExists),
+        pax_id(NoExists),
+        scd_out(NoExists),
+        seats(NoExists),
+        birth_date(NoExists)
+    {}
+
+    bool operator < (const TPFSStatRow &val) const
+    {
+        if(point_id == val.point_id)
+            return pax_id < val.pax_id;
+        return point_id < val.point_id;
+    }
+};
+
+typedef multiset<TPFSStatRow> TPFSStat;
+
+void RunPFSStat(
+        const TStatParams &params,
+        TPFSStat &PFSStat,
+        TPrintAirline &prn_airline
+        )
+{
+    for(int pass = 0; pass <= 2; pass++) {
+        QParams QryParams;
+        QryParams
+            << QParam("FirstDate", otDate, params.FirstDate)
+            << QParam("LastDate", otDate, params.LastDate);
+        if (pass!=0)
+            QryParams << QParam("arx_trip_date_range", otInteger, ARX_TRIP_DATE_RANGE());
+        string SQLText =
+            "select "
+            "   points.point_id, "
+            "   points.scd_out, "
+            "   points.airline, "
+            "   points.flt_no, "
+            "   points.suffix, "
+            "   points.airp, "
+            "   pfs_stat.pax_id, "
+            "   pfs_stat.status, "
+            "   pfs_stat.airp_arv, "
+            "   pfs_stat.seats, "
+            "   pfs_stat.subcls, "
+            "   pfs_stat.pnr, "
+            "   pfs_stat.surname, "
+            "   pfs_stat.name, "
+            "   pfs_stat.gender, "
+            "   pfs_stat.birth_date "
+            "from ";
+        if(pass != 0) {
+            SQLText +=
+                "   arx_pfs_stat pfs_stat, "
+                "   arx_points points ";
+            if(pass == 2)
+                SQLText += ",(SELECT part_key, move_id FROM move_arx_ext \n"
+                    "  WHERE part_key >= :LastDate+:arx_trip_date_range AND part_key <= :LastDate+date_range) arx_ext \n";
+        } else {
+            SQLText +=
+                "   pfs_stat, "
+                "   points ";
+        }
+        SQLText +=
+            "where "
+            "   pfs_stat.point_id = points.point_id and "
+            "   points.pr_del >= 0 and ";
+        params.AccessClause(SQLText);
+        if(params.flt_no != NoExists) {
+            SQLText += " points.flt_no = :flt_no and ";
+            QryParams << QParam("flt_no", otInteger, params.flt_no);
+        }
+        if(pass != 0)
+            SQLText +=
+                " points.part_key = pfs_stat.part_key and ";
+        if(pass == 1)
+            SQLText += " points.part_key >= :FirstDate AND points.part_key < :LastDate + :arx_trip_date_range AND \n";
+        if(pass == 2)
+            SQLText += " points.part_key=arx_ext.part_key AND points.move_id=arx_ext.move_id AND \n";
+        SQLText +=
+            "    points.scd_out >= :FirstDate AND points.scd_out < :LastDate ";
+        TCachedQuery Qry(SQLText, QryParams);
+        Qry.get().Execute();
+        if(not Qry.get().Eof) {
+            int col_point_id = Qry.get().FieldIndex("point_id");
+            int col_scd_out = Qry.get().FieldIndex("scd_out");
+            int col_airline = Qry.get().FieldIndex("airline");
+            int col_flt_no = Qry.get().FieldIndex("flt_no");
+            int col_suffix = Qry.get().FieldIndex("suffix");
+            // int col_airp = Qry.get().FieldIndex("airp");
+            int col_pax_id = Qry.get().FieldIndex("pax_id");
+            int col_status = Qry.get().FieldIndex("status");
+            // int col_airp_arv = Qry.get().FieldIndex("airp_arv");
+            int col_seats = Qry.get().FieldIndex("seats");
+            int col_subcls = Qry.get().FieldIndex("subcls");
+            int col_pnr = Qry.get().FieldIndex("pnr");
+            int col_surname = Qry.get().FieldIndex("surname");
+            int col_name = Qry.get().FieldIndex("name");
+            int col_gender = Qry.get().FieldIndex("gender");
+            int col_birth_date = Qry.get().FieldIndex("birth_date");
+            for(; not Qry.get().Eof; Qry.get().Next()) {
+                prn_airline.check(Qry.get().FieldAsString(col_airline));
+                TPFSStatRow row;
+                row.point_id = Qry.get().FieldAsInteger(col_point_id);
+                row.pax_id = Qry.get().FieldAsInteger(col_pax_id);
+                row.status = Qry.get().FieldAsString(col_status);
+                row.scd_out = Qry.get().FieldAsDateTime(col_scd_out);
+                ostringstream buf;
+                buf
+                    << ElemIdToCodeNative(etAirline, Qry.get().FieldAsString(col_airline))
+                    << setw(3) << setfill('0') << Qry.get().FieldAsInteger(col_flt_no)
+                    << ElemIdToCodeNative(etSuffix, Qry.get().FieldAsString(col_suffix));
+                row.flt = buf.str();
+                /*
+                   buf.str("");
+                   buf
+                   << ElemIdToCodeNative(etAirp, Qry.get().FieldAsString(col_airp)) << "-"
+                   << ElemIdToCodeNative(etAirp, Qry.get().FieldAsString(col_airp_arv));
+                   row.route = buf.str();
+                   */
+
+
+                /*
+                   route.set(GetRouteAfterStr( col_part_key>=0?Qry.FieldAsDateTime(col_part_key):NoExists,
+                   Qry.FieldAsInteger(col_point_id),
+                   trtNotCurrent,
+                   trtNotCancelled),
+                   */
+
+                row.route = GetRouteAfterStr(NoExists, row.point_id, trtWithCurrent, trtNotCancelled);
+
+
+                row.seats = Qry.get().FieldAsInteger(col_seats);
+                row.subcls = ElemIdToCodeNative(etSubcls, Qry.get().FieldAsString(col_subcls));
+                row.pnr = Qry.get().FieldAsString(col_pnr);
+                row.surname = transliter(Qry.get().FieldAsString(col_surname), 1, TReqInfo::Instance()->desk.lang != AstraLocale::LANG_RU);
+                row.name = transliter(Qry.get().FieldAsString(col_name), 1, TReqInfo::Instance()->desk.lang != AstraLocale::LANG_RU);
+
+                row.gender = Qry.get().FieldAsString(col_gender);
+                switch(CheckIn::is_female(row.gender, row.name)) {
+                    case NoExists:
+                        row.gender.clear();
+                        break;
+                    case 0:
+                        row.gender = getLocaleText("CAP.DOC.MALE");
+                        break;
+                    case 1:
+                        row.gender = getLocaleText("CAP.DOC.FEMALE");
+                        break;
+                }
+
+                if(Qry.get().FieldIsNULL(col_birth_date))
+                    row.birth_date = NoExists;
+                else
+                    row.birth_date = Qry.get().FieldAsDateTime(col_birth_date);
+                PFSStat.insert(row);
+            }
+        }
+    }
+}
+
+void createXMLPFSStat(
+        const TStatParams &params,
+        const TPFSStat &PFSStat,
+        const TPrintAirline &prn_airline,
+        xmlNodePtr resNode)
+{
+    if(PFSStat.empty()) throw AstraLocale::UserException("MSG.NOT_DATA");
+    if(PFSStat.size() > MAX_STAT_ROWS) throw MaxStatRowsException("MSG.TOO_MANY_ROWS_SELECTED.RANDOM_SHOWN_NUM.ADJUST_STAT_SEARCH", LParams() << LParam("num", MAX_STAT_ROWS));
+    NewTextChild(resNode, "airline", prn_airline.get(), "");
+    xmlNodePtr grdNode = NewTextChild(resNode, "grd");
+
+    xmlNodePtr headerNode = NewTextChild(grdNode, "header");
+    xmlNodePtr colNode;
+    colNode = NewTextChild(headerNode, "col", getLocaleText("Дата"));
+    SetProp(colNode, "width", 60);
+    SetProp(colNode, "align", TAlignment::LeftJustify);
+    SetProp(colNode, "sort", sortString);
+    colNode = NewTextChild(headerNode, "col", getLocaleText("Рейс"));
+    SetProp(colNode, "width", 50);
+    SetProp(colNode, "align", TAlignment::LeftJustify);
+    SetProp(colNode, "sort", sortString);
+    colNode = NewTextChild(headerNode, "col", getLocaleText("Маршрут"));
+    SetProp(colNode, "width", 100);
+    SetProp(colNode, "align", TAlignment::LeftJustify);
+    SetProp(colNode, "sort", sortString);
+    colNode = NewTextChild(headerNode, "col", getLocaleText("Статус"));
+    SetProp(colNode, "width", 50);
+    SetProp(colNode, "align", TAlignment::LeftJustify);
+    SetProp(colNode, "sort", sortString);
+    colNode = NewTextChild(headerNode, "col", getLocaleText("Мест"));
+    SetProp(colNode, "width", 65);
+    SetProp(colNode, "align", TAlignment::LeftJustify);
+    SetProp(colNode, "sort", sortString);
+    colNode = NewTextChild(headerNode, "col", getLocaleText("RBD"));
+    SetProp(colNode, "width", 30);
+    SetProp(colNode, "align", TAlignment::LeftJustify);
+    SetProp(colNode, "sort", sortString);
+    colNode = NewTextChild(headerNode, "col", getLocaleText("PNR"));
+    SetProp(colNode, "width", 50);
+    SetProp(colNode, "align", TAlignment::LeftJustify);
+    SetProp(colNode, "sort", sortString);
+    colNode = NewTextChild(headerNode, "col", getLocaleText("Фамилия"));
+    SetProp(colNode, "width", 80);
+    SetProp(colNode, "align", TAlignment::LeftJustify);
+    SetProp(colNode, "sort", sortString);
+    colNode = NewTextChild(headerNode, "col", getLocaleText("Имя"));
+    SetProp(colNode, "width", 80);
+    SetProp(colNode, "align", TAlignment::LeftJustify);
+    SetProp(colNode, "sort", sortString);
+    colNode = NewTextChild(headerNode, "col", getLocaleText("Пол"));
+    SetProp(colNode, "width", 30);
+    SetProp(colNode, "align", TAlignment::LeftJustify);
+    SetProp(colNode, "sort", sortString);
+    colNode = NewTextChild(headerNode, "col", getLocaleText("Рождение"));
+    SetProp(colNode, "width", 85);
+    SetProp(colNode, "align", TAlignment::LeftJustify);
+    SetProp(colNode, "sort", sortString);
+
+    xmlNodePtr rowsNode = NewTextChild(grdNode, "rows");
+    xmlNodePtr rowNode;
+    for(TPFSStat::iterator i = PFSStat.begin(); i != PFSStat.end(); i++) {
+        rowNode = NewTextChild(rowsNode, "row");
+        // Дата
+        NewTextChild(rowNode, "col", DateTimeToStr(i->scd_out, "dd.mm.yyyy"));
+        // Рейс
+        NewTextChild(rowNode, "col", i->flt);
+        // Маршрут
+        NewTextChild(rowNode, "col", i->route);
+        // Статус
+        NewTextChild(rowNode, "col", i->status);
+        // Кол-во мест
+        NewTextChild(rowNode, "col", i->seats);
+        // RBD
+        NewTextChild(rowNode, "col", i->subcls);
+        // PNR
+        NewTextChild(rowNode, "col", i->pnr);
+        // Фамилия
+        NewTextChild(rowNode, "col", i->surname);
+        // Имя
+        NewTextChild(rowNode, "col", i->name);
+        // Пол
+        NewTextChild(rowNode, "col", i->gender);
+        // Дата рождения
+        if(i->birth_date == NoExists)
+            NewTextChild(rowNode, "col");
+        else
+            NewTextChild(rowNode, "col", DateTimeToStr(i->birth_date, "dd.mm.yyyy"));
+    }
+
+    xmlNodePtr variablesNode = STAT::set_variables(resNode);
+    NewTextChild(variablesNode, "stat_type", params.statType);
+    NewTextChild(variablesNode, "stat_mode", "PFS");
+    NewTextChild(variablesNode, "stat_type_caption", getLocaleText("Подробная"));
+}
+
+typedef map<string, int> TPFSStatusMap;
+typedef map<string, TPFSStatusMap> TPFSRouteMap;
+typedef map<string, TPFSRouteMap> TPFSFltMap;
+typedef map<TDateTime, TPFSFltMap> TPFSShortStat;
+
+void RunPFSShortStat(
+        const TStatParams &params,
+        TPFSShortStat &PFSShortStat,
+        TPrintAirline &prn_airline
+        )
+{
+    TPFSStat pfs_stat;
+    RunPFSStat(params, pfs_stat, prn_airline);
+    for(TPFSStat::iterator i = pfs_stat.begin(); i != pfs_stat.end(); i++) {
+        PFSShortStat[i->scd_out][i->flt][i->route][i->status]++;
+    }
+}
+
+void createXMLPFSShortStat(
+        const TStatParams &params,
+        TPFSShortStat &PFSShortStat,
+        const TPrintAirline &prn_airline,
+        xmlNodePtr resNode)
+{
+    if(PFSShortStat.empty()) throw AstraLocale::UserException("MSG.NOT_DATA");
+    if(PFSShortStat.size() > MAX_STAT_ROWS) throw MaxStatRowsException("MSG.TOO_MANY_ROWS_SELECTED.RANDOM_SHOWN_NUM.ADJUST_STAT_SEARCH", LParams() << LParam("num", MAX_STAT_ROWS));
+    NewTextChild(resNode, "airline", prn_airline.get(), "");
+    xmlNodePtr grdNode = NewTextChild(resNode, "grd");
+
+    xmlNodePtr headerNode = NewTextChild(grdNode, "header");
+    xmlNodePtr colNode;
+    colNode = NewTextChild(headerNode, "col", getLocaleText("Дата"));
+    SetProp(colNode, "width", 60);
+    SetProp(colNode, "align", TAlignment::LeftJustify);
+    SetProp(colNode, "sort", sortString);
+    colNode = NewTextChild(headerNode, "col", getLocaleText("Рейс"));
+    SetProp(colNode, "width", 50);
+    SetProp(colNode, "align", TAlignment::LeftJustify);
+    SetProp(colNode, "sort", sortString);
+    colNode = NewTextChild(headerNode, "col", getLocaleText("Маршрут"));
+    SetProp(colNode, "width", 100);
+    SetProp(colNode, "align", TAlignment::LeftJustify);
+    SetProp(colNode, "sort", sortString);
+    colNode = NewTextChild(headerNode, "col", getLocaleText("NOREC"));
+    SetProp(colNode, "width", 50);
+    SetProp(colNode, "align", TAlignment::LeftJustify);
+    SetProp(colNode, "sort", sortString);
+    colNode = NewTextChild(headerNode, "col", getLocaleText("NOSHO"));
+    SetProp(colNode, "width", 50);
+    SetProp(colNode, "align", TAlignment::LeftJustify);
+    SetProp(colNode, "sort", sortString);
+    colNode = NewTextChild(headerNode, "col", getLocaleText("GOSHO"));
+    SetProp(colNode, "width", 50);
+    SetProp(colNode, "align", TAlignment::LeftJustify);
+    SetProp(colNode, "sort", sortString);
+
+    xmlNodePtr rowsNode = NewTextChild(grdNode, "rows");
+    xmlNodePtr rowNode;
+    for(TPFSShortStat::iterator
+            stat = PFSShortStat.begin();
+            stat != PFSShortStat.end(); stat++)
+        for(TPFSFltMap::iterator
+                flt = stat->second.begin();
+                flt != stat->second.end(); flt++)
+            for(TPFSRouteMap::iterator
+                    route = flt->second.begin();
+                    route != flt->second.end(); route++)
+            {
+                rowNode = NewTextChild(rowsNode, "row");
+                // Дата
+                NewTextChild(rowNode, "col", DateTimeToStr(stat->first, "dd.mm.yyyy"));
+                // Рейс
+                NewTextChild(rowNode, "col", flt->first);
+                // Маршрут
+                NewTextChild(rowNode, "col", route->first);
+                // NOREC
+                NewTextChild(rowNode, "col", route->second["NOREC"]);
+                // NOSHO
+                NewTextChild(rowNode, "col", route->second["NOSHO"]);
+                // GOSHO
+                NewTextChild(rowNode, "col", route->second["GOSHO"]);
+            }
+
+
+    /*
+    for(TPFSShortStat::iterator i = PFSShortStat.begin(); i != PFSShortStat.end(); i++) {
+        rowNode = NewTextChild(rowsNode, "row");
+        // Дата
+        NewTextChild(rowNode, "col", DateTimeToStr(i->scd_out, "dd.mm.yyyy"));
+        // Рейс
+        NewTextChild(rowNode, "col", i->flt);
+        // Маршрут
+        NewTextChild(rowNode, "col", i->route);
+        // Статус
+        NewTextChild(rowNode, "col", i->status);
+        // Кол-во мест
+        NewTextChild(rowNode, "col", i->seats);
+        // RBD
+        NewTextChild(rowNode, "col", i->subcls);
+        // PNR
+        NewTextChild(rowNode, "col", i->pnr);
+        // Фамилия
+        NewTextChild(rowNode, "col", i->surname);
+        // Имя
+        NewTextChild(rowNode, "col", i->name);
+        // Пол
+        NewTextChild(rowNode, "col", i->gender);
+        // Дата рождения
+        if(i->birth_date == NoExists)
+            NewTextChild(rowNode, "col");
+        else
+            NewTextChild(rowNode, "col", DateTimeToStr(i->birth_date, "dd.mm.yyyy"));
+    }
+    */
+
+    xmlNodePtr variablesNode = STAT::set_variables(resNode);
+    NewTextChild(variablesNode, "stat_type", params.statType);
+    NewTextChild(variablesNode, "stat_mode", "PFS");
+    NewTextChild(variablesNode, "stat_type_caption", getLocaleText("Общая"));
+}
+
+/*---------------------------------------- ---------------------------------------*/
+
 void StatInterface::RunStat(XMLRequestCtxt *ctxt, xmlNodePtr reqNode, xmlNodePtr resNode)
 {
     TReqInfo *reqInfo = TReqInfo::Instance();
@@ -9416,6 +9822,8 @@ void StatInterface::RunStat(XMLRequestCtxt *ctxt, xmlNodePtr reqNode, xmlNodePtr
         case statLimitedCapab:
         case statUnaccBag:
         case statAnnulBT:
+        case statPFSFull:
+        case statPFSShort:
             get_compatible_report_form("stat", reqNode, resNode);
             break;
         default:
@@ -9521,6 +9929,20 @@ void StatInterface::RunStat(XMLRequestCtxt *ctxt, xmlNodePtr reqNode, xmlNodePtr
             TAnnulBTStat AnnulBTStat;
             RunAnnulBTStat(params, AnnulBTStat, airline);
             createXMLAnnulBTStat(params, AnnulBTStat, airline, resNode);
+        }
+        if(params.statType == statPFSFull)
+        {
+            TPrintAirline airline;
+            TPFSStat PFSStat;
+            RunPFSStat(params, PFSStat, airline);
+            createXMLPFSStat(params, PFSStat, airline, resNode);
+        }
+        if(params.statType == statPFSShort)
+        {
+            TPrintAirline airline;
+            TPFSShortStat PFSShortStat;
+            RunPFSShortStat(params, PFSShortStat, airline);
+            createXMLPFSShortStat(params, PFSShortStat, airline, resNode);
         }
     }
     /* GRISHA */
@@ -10628,6 +11050,7 @@ void get_flight_stat(int point_id, bool final_collection)
      get_service_stat(point_id);
      get_limited_capability_stat(point_id);
      get_kuf_stat(point_id);
+     get_pfs_stat(point_id);
    };
 
    TReqInfo::Instance()->LocaleToLog("EVT.COLLECT_STATISTIC", evtFlt, point_id);
