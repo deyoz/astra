459f8748ff1c213e5c3e75165f0c83a0b2bdc615
Anton Bokov <a.bokov@sirena2000.ru>
2017-04-14 13:30:25 +0000

    #28554 - yet another fix
    git-svn-id: svn+ssh://10.1.90.223/SVNroot/astra/server/trunk@192948 e9a6705e-e829-0410-83f2-cebdff2db7e5

--- src/tlg/edi_handler.cpp
+++ src/tlg/edi_handler.cpp
@@ -23,6 +23,7 @@
 #include <serverlib/posthooks.h>
 #include <serverlib/ourtime.h>
 #include <serverlib/TlgLogger.h>
+#include <serverlib/testmode.h>
 #include <edilib/edi_func_cpp.h>
 #include <libtlg/telegrams.h>
 
@@ -174,6 +175,7 @@ void handle_edi_tlg(const tlg_info &tlg)
         ASTRA::commit();
         callPostHooksAfter();
         emptyHookTables();
+        if(!inTestMode()) throw;
     }
     catch(edifact::edi_exception &e)
     {
@@ -269,6 +271,11 @@ bool handle_tlg(void)
         handle_edi_tlg(tlgi);
       };
       queue_not_empty=!TlgQry.Eof;
+  }
+  catch(TlgHandling::TlgToBePostponed& e)
+  {
+    tst();
+    queue_not_empty = true;
   }
   catch(...)
   {
