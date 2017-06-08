2b109568083ecaeaeddf90b6433af8b8a1430fab
Grigory Koshkin <g.koshkin@sirena2000.ru>
2017-06-08 13:18:41 +0000

    28001 hot fix
    git-svn-id: svn+ssh://10.1.90.223/SVNroot/astra/server/trunk@194798 e9a6705e-e829-0410-83f2-cebdff2db7e5

--- src/stat.cc
+++ src/stat.cc
@@ -3010,6 +3010,13 @@ struct TFullCmp {
 };
 typedef map<TFullStatKey, TFullStatRow, TFullCmp> TFullStat;
 
+namespace AstraLocale {
+class MaxStatRowsException: public UserException
+{
+    public: MaxStatRowsException(const std::string &vlexema, const LParams &aparams): UserException(vlexema, aparams) {}
+};
+}
+
 // TODO переименовать здесь и всюду bool full в что-то типа override_MAX_STAT_ROWS()
 template <class keyClass, class rowClass, class cmpClass>
 void AddStatRow(const keyClass &key, const rowClass &row, map<keyClass, rowClass, cmpClass> &stat, bool full = false)
@@ -3019,6 +3026,8 @@ void AddStatRow(const keyClass &key, const rowClass &row, map<keyClass, rowClass
     i->second+=row;
   else
   {
+    if ((not full) and (stat.size() > MAX_STAT_ROWS()))
+      throw MaxStatRowsException("MSG.TOO_MANY_ROWS_SELECTED.RANDOM_SHOWN_NUM.ADJUST_STAT_SEARCH", LParams() << LParam("num", MAX_STAT_ROWS()));
     if (full or stat.size()<=MAX_STAT_ROWS())
       stat.insert(make_pair(key,row));
   };
@@ -3284,13 +3293,6 @@ void setClientTypeCaps(xmlNodePtr variablesNode)
     NewTextChild(variablesNode, "pax", getLocaleText("CAP.PAX"));
     NewTextChild(variablesNode, "mob", getLocaleText("CAP.MOB"));
     NewTextChild(variablesNode, "mobile_devices", getLocaleText("CAP.MOBILE_DEVICES"));
-}
-
-namespace AstraLocale {
-class MaxStatRowsException: public UserException
-{
-    public: MaxStatRowsException(const std::string &vlexema, const LParams &aparams): UserException(vlexema, aparams) {}
-};
 }
 
 void createXMLDetailStat(const TStatParams &params, bool pr_pact,
