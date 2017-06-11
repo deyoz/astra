2f367c8500913d01a25703693184c40526c0a0f1
Denis Zaharaov <denis_zaharov@sirena2000.ru>
2017-06-11 18:50:31 +0000

    merge -c194861 from trunk #28592: extend bi_print_rules: rfisc field added into bi_print_rules
    git-svn-id: svn+ssh://10.1.90.223/SVNroot/astra/server/stand@194862 e9a6705e-e829-0410-83f2-cebdff2db7e5

--- src/print.cc
+++ src/print.cc
@@ -2150,6 +2150,7 @@ void PrintInterface::GetPrintDataBP(XMLRequestCtxt *ctxt, xmlNodePtr reqNode, xm
     if(TReqInfo::Instance()->desk.compatible(OP_TYPE_VERSION)) {
         bool bi_access = false;
         for (std::vector<BPPax>::iterator iPax=paxs.begin(); iPax!=paxs.end(); ++iPax ) {
+            if(first_seg_grp_id != iPax->grp_id) continue;
 
             LogTrace(TRACE5) << "bi_rules.get pax_id = " << iPax->pax_id;
 
