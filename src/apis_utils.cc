c501543fb450f695b6059c9f4a5671baf54c4299
Grigory Koshkin <g.koshkin@sirena2000.ru>
2017-08-24 11:53:18 +0000

    #29899 merge -c197460 from stand
    git-svn-id: svn+ssh://10.1.90.223/SVNroot/astra/server/stable@197498 e9a6705e-e829-0410-83f2-cebdff2db7e5

--- src/apis_utils.cc
+++ src/apis_utils.cc
@@ -1058,10 +1058,20 @@ void HandleDoco(const CheckIn::TPaxGrpItem &grp,
 void HandleDoca(const CheckIn::TPaxGrpItem &grp,
                 const CheckIn::TSimplePaxItem &pax,
                 const TCompleteAPICheckInfo &checkInfo,
-                CheckIn::TDocaMap &doca_map)
+                const CheckIn::TDocaMap &doca_map)
 {
   ASTRA::TPaxTypeExt pax_type_ext(grp.status, pax.crew_type);
-  for(CheckIn::TDocaMap::iterator d = doca_map.begin(); d != doca_map.end(); ++d)
+  // При вкл. настройке "не контролировать документ у экипажа / доп. экипажа"
+  // не производится проверка заполнения обязательных полей для используемого APIS,
+  // таким образом, становится возможным отсутствие документа в контейнере,
+  // из-за чего проверка incomplete не производится.
+  // Для обхода этой ситуации введён дополнительный контейнер, содержащий в том числе и пустые документы
+  // (с заполненным типом, во избежание Exception: NormalizeDoca: result.apiType()==apiUnknown!)
+  CheckIn::TDocaMap doca_map_2(doca_map);
+  if (!doca_map_2.count(apiDocaB)) doca_map_2[apiDocaB].type = "B";
+  if (!doca_map_2.count(apiDocaR)) doca_map_2[apiDocaR].type = "R";
+  if (!doca_map_2.count(apiDocaD)) doca_map_2[apiDocaD].type = "D";
+  for(CheckIn::TDocaMap::iterator d = doca_map_2.begin(); d != doca_map_2.end(); ++d)
   {
     CheckIn::TPaxDocaItem &doc = d->second;
     doc=NormalizeDoca(doc);
