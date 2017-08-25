32a657d37d989476dbedd41222a19900715c5f82
Grigory Shpakov <g.shpakov@sirena2000.ru>
2017-08-25 09:45:09 +0000

    Разделил написанное на 3 части:    bagmessage.cc - то, что непосредственно связано с протоколом связи и собственно связью    bagmessage_daemon.cc - то, что связано с очередью телеграмм на отправку и организацией отправки телеграм    bagmessage.h - то, что нужно обоим этим файлам    Попутно переписал немного работу с принятыми сообщениями. Правда, проверить не на чем.
    git-svn-id: svn+ssh://10.1.90.223/SVNroot/astra/server/trunk@197552 e9a6705e-e829-0410-83f2-cebdff2db7e5

--- src/bagmessage.cc
+++ src/bagmessage.cc
@@ -1,48 +1,20 @@
 #include <boost/asio.hpp>
 #include <boost/bind.hpp>
-#include <serverlib/new_daemon.h>
-#include <serverlib/tclmon.h>
 
-#include "exceptions.h"
-#include "msg_queue.h"
 #include "astra_utils.h"
-#include "file_queue.h"
-#include "date_time.h"
 
 #define NICKNAME "GRIG"
 #define NICKTRACE GRIG_TRACE
 #include <serverlib/slogger.h>
 
-using namespace EXCEPTIONS;
 using namespace std;
 using namespace boost::asio;
-using namespace BASIC::date_time;
 
-class BagMsgQueueDaemon : public ServerFramework::NewDaemon
-{
-public:
-    BagMsgQueueDaemon() : ServerFramework::NewDaemon("bag_msg_handler") {}
-};
+#include "bagmessage.h"
 
 /**************************************************************************/
 /* Заголовок сообщения, отправляемого в BagMessage или получаемого оттуда */
 /**************************************************************************/
-class BM_HEADER
-{
-  public:
-  // Элементы структуры
-    string appl_id;                // Идентификация клиента - 8 символов
-    unsigned version;              // Версия заголовка - 2-байтовое беззнаковое
-    unsigned type;                 // Тип сообщения - 2-байтовое беззнаковое
-    unsigned message_id_number;    // Идентификационный номер сообщения - 2-байтовое беззнаковое
-    unsigned data_length;          // Длина текстовых данных после заголовка - 2-байтовое беззнаковое
-                                   // И еще 4 байта зарезервированы
-  // Константы и методы
-    static const int SIZE = 20;    // Длина передаваемого заголовка в байтах
-    void loadFrom( char *buf );    // Загрузить заголовок из буфера после приема
-    void saveTo( char *buf );      // Записать заголовок в буфер перед передачей
-};
-
 void BM_HEADER::loadFrom( char *buf )
 {
   appl_id = string( buf, 8 );
@@ -67,195 +39,21 @@ void BM_HEADER::saveTo( char *buf )
   buf[16] = buf[17] = buf[18] = buf[19] = 0;
 }
 
-/*****************************/
-/* Входная очередь сообщений */
-/*****************************/
-// Сообщение вместе с заголовком.
-typedef struct
-{
-  BM_HEADER head;
-  string text;
-} BM_MESSAGE;
-
-std::queue<BM_MESSAGE> mes_queue;
-
-/******************************/
-/* Выходная очередь сообщений */
-/******************************/
-class BM_OUTPUT_QUEUE : public TFileQueue
-{
-  private:
-    string canonName;                      // Имя сервиса, по которому отправляются телеграммы
-    static const int WAIT_ANSWER_SEC = 30; // Тайм-аут между отметками 'send' и 'done'. Если истек - сообщение подлежит повторной отсылке
-    time_t lastRead;                       // Когда последний раз читали очередь из базы
-  public:
-    BM_OUTPUT_QUEUE();                     // Конструктор
-    void get();                            // Перечитать очередь
-    void sendFile( int tlg_id );           // Поставить отметку "телеграмма взята на отсылку"
-    static void unSendFile( int tlg_id );  // Снять отметку "телеграмма взята на отсылку"
-    static void doneFile( int tlg_id );    // Поставить отметку "телеграмма доставлена"
-    static void onWriteFinished( int tlg_id, int result ); // Обработчик завершения отправки телеграммы
-};
-
-BM_OUTPUT_QUEUE::BM_OUTPUT_QUEUE()
-{
-  canonName = getTCLParam( "SBM_CANON_NAME", NULL );
-  ProgTrace( TRACE0, "SITA BagMessage: canon_name=%s", canonName.c_str() );
-  lastRead = 0;
-}
-
-void BM_OUTPUT_QUEUE::get()
-{
-  time_t now = time( NULL );
-//  if( now - lastRead > 0 ) // Чтобы не перечитывать базу по несколько раз в секунду
-  {
-    ProgTrace( TRACE5, "re-reading outgoing queue" );
-    TFileQueue::get( TFilterQueue( canonName, WAIT_ANSWER_SEC ) );
-    lastRead = now;
-  }
-}
-
-void BM_OUTPUT_QUEUE::sendFile( int tlg_id )
-{
-  TFileQueue::sendFile( tlg_id );
-  OraSession.Commit();
-  lastRead = lastRead - 2; // Для обмана защиты от повторного чтения
-  get();
-}
-
-void BM_OUTPUT_QUEUE::unSendFile( int tlg_id )
-{
-  TQuery Qry(&OraSession);
-  Qry.SQLText = "UPDATE file_queue SET status='PUT', time=system.UTCSYSDATE WHERE id= :id";
-  Qry.CreateVariable( "id", otInteger, tlg_id );
-  Qry.Execute();
-  bool res = ( Qry.RowsProcessed() > 0 );
-  if ( res ) {
-    ProgTrace( TRACE5, "unSendFile id=%d", tlg_id );
-  }
-  OraSession.Commit();
-}
-
-void BM_OUTPUT_QUEUE::doneFile( int tlg_id )
-{
-  TFileQueue::doneFile( tlg_id );
-  OraSession.Commit();
-}
-
-void BM_OUTPUT_QUEUE::onWriteFinished( int tlg_id, int error )
-{
-  if( error == 0 )
-  {
-    ProgTrace( TRACE5, "Message id=%d sent OK", tlg_id );
-    doneFile( tlg_id );
-  }
-  else
-  {
-    ProgError( STDLOG, "Failed to send message to SITA BagMessage! tlg_id=%d", tlg_id );
-    unSendFile( tlg_id );
-  }
-}
-
 /*****************************************/
 /* Соединение с системой SITA BagMessage */
 /*****************************************/
 // Расшифровки типов сообщений, которые могут быть посланы в систему SITA BagMessage
-const string messageTypes[] = { "unknown", "LOGIN_RQST", "LOGIN_ACCEPT", "LOGIN_REJECT", "DATA", "ACK_DATA",
-                                "ACK_MSG", "NAK_MSG", "STATUS", "DATA_ON", "DATA_OFF", "LOG_OFF" };
 
-class BMConnection
+const string BMConnection::messageTypes[12] = { "unknown", "LOGIN_RQST", "LOGIN_ACCEPT", "LOGIN_REJECT", "DATA", "ACK_DATA",
+                                                "ACK_MSG", "NAK_MSG", "STATUS", "DATA_ON", "DATA_OFF", "LOG_OFF" };
+
+BMConnection::BMConnection( int i, boost::asio::io_service& io ) : socket( io )
 {
-  public:
-  // Состояние асинхронной операции
-    typedef enum
-    {
-      BM_OP_NONE = 0,      // Не было попытки сделать операцию
-      BM_OP_WAIT = 1,      // Операция начата - ждем завершения
-      BM_OP_READY = 2      // Закончено нормально
-    } BM_OPERATION_STATUS;
-  // Типы сообщений, которые могут быть посланы в систему SITA BagMessage или приняты из нее.
-    typedef enum
-    {
-      LOGIN_RQST = 1,      // Запрос входа в удаленную систему
-      LOGIN_ACCEPT = 2,    // Положительный ответ на запрос входа
-      LOGIN_REJECT = 3,    // Отрицательный ответ на запрос входа
-      DATA = 4,            // Передача данных
-      ACK_DATA = 5,        // Передача данных с запросом подтверждения приема
-      ACK_MSG = 6,         // Положительный ответ на запрос подтверждения приема данных
-      NAK_MSG = 7,         // Отрицательный ответ на запрос подтверждения приема данных
-      STATUS = 8,          // Статус - подтверждение того, что связь не нарушена
-      DATA_ON = 9,         // Отмена ранее выданного DATA_OFF
-      DATA_OFF = 10,       // Просьба приостановить посылку данных
-      LOG_OFF = 11         // Запрос закрытия связи
-    } BM_MESSAGE_TYPE;
-  // Версии заголовков сообщения
-    typedef enum
-    {
-      VERSION_2 = 2        // Пока что только такое
-    } BM_HEADER_VERSIONS;
-  private:
-    typedef struct
-    {
-      string host;        // IP-адрес хоста
-      int port;           // Порт
-    } BM_HOST;
-    static const int NUMLINES = 2;
-  // Настройки, прочитанные извне при запуске
-    int line_number;   // Порядковый номер соединения в системе
-    BM_HOST ip_addr[NUMLINES];  // Адреса для линий связи - основной и резервный (резервнЫЕ?)
-    string login;      // Логин в системе SITA BagMessage
-    string password;   // Пароль там же
-    int heartBeat;     // Максимальный интервал времени между сообщениями
-  // Рабочие переменные
-  // Статусы
-    bool configured;   // Признак того, что конфигурация прочитана
-    bool paused;       // Признак того, что другая сторона запросила паузу (выдала DATA_OFF)
-    BM_OPERATION_STATUS connected;    // Признак того, что связь установлена
-    BM_OPERATION_STATUS loginStatus;  // Признак того, что мы вошли в ту систему
-    BM_OPERATION_STATUS writeStatus;  // Состояние последней операции передачи данных
-  // Связь и протокольные дела
-    ip::tcp::socket socket;  // Сокет для связи
-    int activeIp;        // Какая из линий связи сейчас используется?
-    unsigned mes_num;    // Номер сообщения для протокола связи
-    time_t lastAdmin;    // Время последнего действия по управлению процессом - для организации задержек
-  // Прием
-    time_t lastRecvTime; // Время прихода предыдущего сообщения - отслеживается для оценки работоспособности линии
-    char *rbuf;          // Буфер для функции async_read
-    BM_HEADER rheader;   // Заголовок принятого сообщения
-    int needSendAck;     // Нужно передать подтверждение принятого сообщения с этим номером
-  // Передача
-    time_t lastSendTime; // Время передачи предыдущего сообщения - для передачи статуса при затишье
-    BM_HEADER header;    // Заголовок сообщения, формируемого для передачи
-    string text;         // Текст сообщения, формируемого для передачи
-    char *wbuf;          // Буфер для функции async_write
-    int waitForAck;      // Протокольный номер сообщения, при отсылке которого выставлен запрос подтверждения,
-                         // ответ на который еще не получен, или -1 если ничего не ждем
-    int waitForAckId;    // ID соответствующей телеграммы в базе данных
-    time_t waitForAckTime; // Когда послали это сообщение
-    void (*writeHandler)( int tlg_id, int status ); // Обработчик завершения передачи с подтверждением
-  public:
-    BMConnection( int i, io_service& io ) : socket( io ) { line_number = i; configured = false; wbuf = NULL; rbuf = NULL; }
-    ~BMConnection();
-    void run();          // Сделать что-то в рабочем цикле
-  private:
-    bool isInit() { return configured; };      // Проверить, что конфигурация прочитана
-    void init();                               // Прочитать конфигурацию и сохранить в объекте
-    void connect();                            // Подключиться к серверу
-    void onConnect( const boost::system::error_code& err );   // Обработчик завершения операции подключения
-    void onRead( const boost::system::error_code& error, std::size_t n ); // Обработчик завершения операции приема сообщения
-    void onWrite( const boost::system::error_code& error, std::size_t n ); // Обработчик завершения операции посылки сообщения
-    bool makeMessage( BM_MESSAGE_TYPE type, string text = "" , int msg_id = -1 );     // Подготовить заголовок и текст к отсылке
-    void doSendMessage();                                      // Отослать подготовленное сообщение
-    void sendMessage( BM_MESSAGE_TYPE type, string text = "", int msg_id = -1 );  // Послать в BagMessage сообщение - общие дела
-    void sendLogin();                     // Послать в ту систему логин и пароль
-    void checkInput();                    // Проверить входной поток и что-то с ним сделать
-    void checkTimer();                    // Проверить таймеры сообщений - вдруг линия грохнулась
-  public:
-    int getNumber() { return line_number; }    // Выдать номер соединения наружу
-    void sendTlg( string text );               // Послать BSM-телеграмму
-    void sendTlgAck( int id, string text, void (*handler)( int, int) );  // Послать BSM-телеграмму с запросом подтверждения приема
-    bool readyToSend();                        // Проверить, что соединение доступно для отсылки сообщений
-};
+  line_number = i;
+  configured = false;
+  wbuf = NULL;
+  rbuf = NULL;
+}
 
 BMConnection::~BMConnection()
 {
@@ -491,7 +289,7 @@ void BMConnection::onRead( const boost::system::error_code& error, std::size_t n
       mes.text = string( rbuf, rheader.data_length );
       delete( rbuf );
       rbuf = NULL;
-      mes_queue.push( mes ); // НЕХОРОШО - используем глобальную переменную mes_queue. Надо бы переделать
+      inputQueue.push( mes );
     }
     else
     {
@@ -659,111 +457,14 @@ bool BMConnection::readyToSend()
   return result;
 }
 
-/*****************************************/
-/* Обработка очереди пришедших сообщений */
-/*****************************************/
-void processBagMessageMes( BM_MESSAGE& mes )
-{ // Если для пришедшего сообщения нужна какая-то своя обработка - то вставить это сюда
-  // А пока что просто трассировка
-  ProgTrace( TRACE5, "processBagMessageMes: text=%s", mes.text.c_str() );
+bool BMConnection::readyToReceive()
+{
+  return ! inputQueue.empty();
 }
 
-/****************************/
-/* Основные рабочие функции */
-/****************************/
-typedef std::shared_ptr<BMConnection> pBMConnection;
-
-void run_bag_msg_process( const std::string &name )
+BM_MESSAGE BMConnection::getFirst()
 {
-  io_service io;
-  BM_OUTPUT_QUEUE queue;
-  int NUM_CONNECTIONS = 1; // Количество параллельных соединений; в будущем может быть увеличено;
-// Также в будущем возможно чтение этой величины из конфигурационного файла
-//  NUM_CONNECTIONS = getTCLParam( "SBM_NUM_CONNECTIONS", 1, 4, 1 );
-  ProgTrace( TRACE0, "SITA BagMessage: NUM_CONNECTIONS=%d", NUM_CONNECTIONS );
-  vector<pBMConnection> conn;
-  for( int i = 0; i < NUM_CONNECTIONS; ++i )
-    conn.push_back( pBMConnection( new BMConnection( i, io ) ) );
-  vector<pBMConnection>::iterator lastSend = conn.begin();
-
-  for(;;)
-  {
-    if( io.stopped() ) // Обязательно! Иначе сервис дойдет до конца своей очереди, остановится и больше ничего делать не будет.
-      io.reset();
-  // Текущая работа соединений
-    for( vector<pBMConnection>::iterator curr = conn.begin(); curr != conn.end(); ++curr )
-      (*curr)->run();
-  // Текущая работа самого boost'а
-    io.poll_one();
-  // Очередь на выход
-    queue.get();
-    if( ! queue.empty() )
-    {
-      TQueueItem tlg = *queue.begin();
-      ProgTrace( TRACE5, "first in queue: id=%d,receiver=%s,type=%s,time=%s",
-                 tlg.id, tlg.receiver.c_str(), tlg.type.c_str(), DateTimeToStr( tlg.time, "yyyy-mm-dd hh:nn:ss" ).c_str() );
-      vector<pBMConnection>::iterator curr = lastSend + 1;
-      if( curr == conn.end() )
-        curr = conn.begin();
-      unsigned n = 0;
-      for( ; n < conn.size(); n++ )
-      {
-        if( (*curr)->readyToSend() )
-        {
-          ProgTrace( TRACE5, "send message via connection %d", (*curr)->getNumber() );
-          queue.sendFile( tlg.id );
-#if 1
-// Пока что работаем в режиме "с подтверждением"
-          (*curr)->sendTlgAck( tlg.id, tlg.data, BM_OUTPUT_QUEUE::onWriteFinished );
-#else
-// А если будем слать без подтверждения - тогда так:
-          (*curr)->sendTlg( tlg.data );
-          BM_OUTPUT_QUEUE::doneFile( tlg.id );
-#endif
-          lastSend = curr;
-          break;
-        }
-        if( curr + 1 == conn.end() )
-        {
-          curr = conn.begin();
-        }
-        else
-        {
-          curr = curr + 1;
-        }
-      }
-      if( n == conn.size() )
-      {
-        ProgTrace( TRACE5, "No ready connections to send message" );
-      }
-    }
-  // Очередь на вход
-    if( ! mes_queue.empty() )
-    {
-      BM_MESSAGE mes = mes_queue.front();
-      mes_queue.pop();
-      processBagMessageMes( mes );
-    }
-  } // Конец основного рабочего цикла
-}
-
-int main_bag_msg_handler_tcl( int supervisorSocket, int argc, char *argv[] )
-{
-  try
-  {
-    sleep(2); // !!!vlad это необходимо чтобы cores не забивали дисковое пространство
-    BagMsgQueueDaemon daemon;
-    run_bag_msg_process( argc > 1 ? argv[1] : "" );
-  }
-  catch( std::exception &E )
-  {
-    ProgError( STDLOG, "std::exception: %s", E.what() );
-    throw;
-  }
-  catch( ... )
-  {
-    ProgError( STDLOG, "Unknown error" );
-    throw;
-  };
-  return 0;
+  BM_MESSAGE mes = inputQueue.front();
+  inputQueue.pop();
+  return mes;
 }
