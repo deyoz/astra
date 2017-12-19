5e63802ca7a997e42aeb975bd26ad9812075cba1
Grigory Koshkin <g.koshkin@sirena2000.ru>
2017-12-19 11:30:52 +0000

    #29932 code convert
    git-svn-id: svn+ssh://10.1.90.223/SVNroot/astra/server/stand@202339 e9a6705e-e829-0410-83f2-cebdff2db7e5

--- src/astra_elem_utils.h
+++ src/astra_elem_utils.h
@@ -11,8 +11,9 @@ enum TCheckFieldFromXML {cfNothingIfEmpty, cfTraceIfEmpty, cfErrorIfEmpty};
 
 std::string airl_fromXML(xmlNodePtr node, TCheckFieldFromXML check_type, const std::string &trace_info);
 std::string airl_fromXML(const std::string &value, TCheckFieldFromXML check_type, const std::string &trace_info, const std::string &node_name);
-std::string airp_fromXML(xmlNodePtr node, TCheckFieldFromXML check_type, const std::string &trace_info);
-std::string airp_fromXML(const std::string &value, TCheckFieldFromXML check_type, const std::string &trace_info, const std::string &node_name);
+std::string airp_fromXML(xmlNodePtr node, TCheckFieldFromXML check_type, const std::string &trace_info, const std::string& system_name = "");
+std::string airp_fromXML(const std::string &value, TCheckFieldFromXML check_type, const std::string &trace_info, const std::string &node_name,
+                         const std::string& system_name = "");
 int flt_no_fromXML(std::string str);
 std::string  suffix_fromXML(std::string str);
 TDateTime scd_out_fromXML(std::string str, const char* fmt);
