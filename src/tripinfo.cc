4f173957eab43a9da826fd4d261eedd3f6a680e1
Grigory Koshkin <g.koshkin@sirena2000.ru>
2017-12-26 13:20:51 +0000

    #29377 mark pax docs
    git-svn-id: svn+ssh://10.1.90.223/SVNroot/astra/server/trunk@202528 e9a6705e-e829-0410-83f2-cebdff2db7e5

--- src/tripinfo.cc
+++ src/tripinfo.cc
@@ -2603,7 +2603,7 @@ void viewPaxLoadSectionReport(int point_id, xmlNodePtr resNode )
   //TQuery StagesQry( &OraSession );
   Qry.SQLText =
     "SELECT " + TTripInfo::selectedFields() + "litera, bort, trip_type "
-    "FROM points WHERE point_id=:point_id AND pr_del>=0"; 
+    "FROM points WHERE point_id=:point_id AND pr_del>=0";
   Qry.CreateVariable("point_id", otInteger, point_id);
   Qry.Execute();
   if ( Qry.Eof ) {
@@ -3179,7 +3179,7 @@ void viewCRSList( int point_id, xmlNodePtr dataNode )
 
 
 template <class T1>
-std::string getDocsFlag( const T1 &crs_pax_doc, const T1 &pax_doc, bool pr_checkin, std::string flagStr, bool firstFlag )
+std::string getDocsFlag_OLD( const T1 &crs_pax_doc, const T1 &pax_doc, bool pr_checkin, std::string flagStr, bool firstFlag )
 {
   std::string res;
   if ( (crs_pax_doc.empty() && !pax_doc.empty()) ||
@@ -3196,6 +3196,44 @@ std::string getDocsFlag( const T1 &crs_pax_doc, const T1 &pax_doc, bool pr_check
     res += string("-") + flagStr;
   }
   return res;
+}
+
+template <class TDOC>
+std::string getDocsFlag( const TDOC &crs_pax_doc, const TDOC &pax_doc, bool pr_checkin, std::string flagStr, bool firstFlag )
+{
+  ostringstream res;
+  if (!firstFlag) res << "/";
+  if (crs_pax_doc.empty())
+  {
+    if (pax_doc.empty())
+    {
+      return "";
+    }
+    else
+    {
+      if (pax_doc.scanned_attrs == NO_FIELDS)
+        res << "+";
+      else
+        res << "*";
+    }
+  }
+  else // !crs_pax_doc.empty()
+  {
+    if (pax_doc.empty())
+    {
+      if (pr_checkin)
+        res << "-";
+    }
+    else if (!crs_pax_doc.equalAttrs( pax_doc ))
+    {
+      if (pax_doc.scanned_attrs == NO_FIELDS)
+        res << "#";
+      else
+        res << "=";
+    }
+  }
+  res << flagStr;
+  return res.str();
 }
 
 std::string getDocsFlags( int pax_id, bool pr_checkin )
