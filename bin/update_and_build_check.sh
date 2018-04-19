e02c4302a8981893cb20e9e4b7ee5aded0e67718
Anton Bokov <a.bokov@sirena2000.ru>
2018-04-19 11:50:09 +0000

    #tech - libcheck version 12
    git-svn-id: svn+ssh://10.1.90.223/SVNroot/astra/server/trunk@206699 e9a6705e-e829-0410-83f2-cebdff2db7e5

--- bin/update_and_build_check.sh
+++ bin/update_and_build_check.sh
@@ -1,20 +1,20 @@
 #!/bin/bash -e
 
-function uab_check_version() {
+uab_check_version() {
     v=`grep 'CHECK_M...._VERSION' $1/include/check.h | cut -f3 -d\  | tr '\n' ' '`
-    test "$v" == "(0) (10) (0) "
+    test "$v" == "(0) (12) (0) "
 }
 
-function uab_config_and_build() {
+uab_config_and_build() {
     prefix=${1:?prefix as the 1st parameter}
     shift 1
     autoreconf --force --install
-    ./configure --prefix=$prefix $@
+    ./configure --prefix=$prefix "$@"
     make -j${MAKE_J:-3}
     make install
 }
 
-function uab_pkg_tarball() {
-    echo check-0.10.0.tar.gz
+uab_pkg_tarball() {
+    echo check-0.12.0.tar.gz
 }
 
