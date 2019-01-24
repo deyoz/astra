e3705bad6cdf21e9fe4bebb7098f84fa3c47366f
Denis Zaharaov <denis_zaharov@sirena2000.ru>
2019-01-24 12:29:27 +0000

    merge -c-216781 .; merge -c216788 from stand: #33684: CBBG in pax manifest: sort by airp_arv fix
    git-svn-id: svn+ssh://10.1.90.223/SVNroot/astra/server/stable@216789 e9a6705e-e829-0410-83f2-cebdff2db7e5

---
+++ src/docs/consts.h
@@ -0,0 +1,6 @@
+#ifndef _DOCS_CONSTS_H_
+#define _DOCS_CONSTS_H_
+
+enum TSortType {stRegNo, stSurname, stSeatNo, stServiceCode};
+
+#endif
--- src/stat_fv.cc
+++ src/stat_fv.cc
@@ -8,7 +8,7 @@
 #include "astra_locale.h"
 #include "qrys.h"
 #include "passenger.h"
-#include "docs.h"
+#include "docs/common.h"
 
 #define NICKNAME "DENIS"
 #include "serverlib/slogger.h"
