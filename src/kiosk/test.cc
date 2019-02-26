d7575327dabdff83b2f37c55f81ae5b7820ab437
Denis Zaharaov <denis_zaharov@sirena2000.ru>
2019-02-26 11:49:52 +0000

    #34404: kiosk subdir
    git-svn-id: svn+ssh://10.1.90.223/SVNroot/astra/server/trunk@217775 e9a6705e-e829-0410-83f2-cebdff2db7e5

---
+++ src/kiosk/test.cc
@@ -0,0 +1,9 @@
+#include "test.h"
+
+#define NICKNAME "DENIS"
+#include "serverlib/slogger.h"
+
+void kiosk_test()
+{
+    LogTrace(TRACE5) << "this is kiosk_test";
+}
---
+++ src/kiosk/test.h
@@ -0,0 +1,6 @@
+#ifndef _KIOSK_COMMON_H_
+#define _KIOSK_COMMON_H_
+
+void kiosk_test();
+
+#endif
