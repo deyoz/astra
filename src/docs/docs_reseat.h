61458efa69e474d354e75bbfb02b04fde28fe102
Denis Zaharaov <denis_zaharov@sirena2000.ru>
2019-07-05 11:02:12 +0000

    merge -c222512 from trunk; #27109: reseat: seat_no lang = comp lang
    git-svn-id: svn+ssh://10.1.90.223/SVNroot/astra/server/stand@222513 e9a6705e-e829-0410-83f2-cebdff2db7e5

---
+++ src/docs/docs_reseat.h
@@ -0,0 +1,9 @@
+#ifndef _DOCS_RESEAT_H_
+#define _DOCS_RESEAT_H_
+
+#include "docs_common.h"
+
+void RESEAT(TRptParams &rpt_params, xmlNodePtr reqNode, xmlNodePtr resNode);
+void RESEATTXT(TRptParams &rpt_params, xmlNodePtr reqNode, xmlNodePtr resNode);
+
+#endif
