764719fe0c0221d5ffdf4127cc0c196d5df0fd35
Chicherov Vlad <vlad@sirena2000.ru>
2019-09-13 18:10:08 +0000

    #35338 China IAPI testing - mirroring incoming CUSRES
    git-svn-id: svn+ssh://10.1.90.223/SVNroot/astra/server/trunk@225219 e9a6705e-e829-0410-83f2-cebdff2db7e5

--- src/tlg/CusRequestHandler.cpp
+++ src/tlg/CusRequestHandler.cpp
@@ -40,21 +40,59 @@ void CusRequestHandler::parse()
 void CusRequestHandler::handle()
 {
     ASSERT(m_data);
-    ProcessChinaCusres(*m_data); 
+    ProcessChinaCusres(*m_data);
 }
 
 void CusRequestHandler::makeAnAnswer()
 {
+    ASSERT(m_data);
+    const Cusres& cusres = *m_data;
+
     viewBgmElement(pMesW(), BgmElem("312", ""));
-     
-    PushEdiPointW(pMesW());
-    SetEdiSegGr(pMesW(), SegGrElement(4));
-    SetEdiPointToSegGrW(pMesW(), SegGrElement(4), "SegGr4 not found");
-    
-    SetEdiFullSegment(pMesW(), SegmElement("ERP"), "1"); // TODO
-    SetEdiFullSegment(pMesW(), SegmElement("ERC"), "1"); // TODO   
-    
-    PopEdiPointW(pMesW());
+
+    if (cusres.m_rff)
+      viewRffElement( pMesW(), cusres.m_rff.get() );
+
+    int segmGroupNum=0;
+    for(const Cusres::SegGr3& sg3 : cusres.m_vSegGr3)
+    {
+      SetEdiSegGr( pMesW(), SegGrElement( 3, segmGroupNum ) );
+
+      PushEdiPointW( pMesW() );
+      SetEdiPointToSegGrW( pMesW(), SegGrElement( 3, segmGroupNum ) );
+
+      viewRffElement( pMesW(), sg3.m_rff );
+
+      viewDtmElement( pMesW(), sg3.m_dtm1, 0 );
+      viewDtmElement( pMesW(), sg3.m_dtm2, 1 );
+      viewLocElement( pMesW(), sg3.m_loc1, 0 );
+      viewLocElement( pMesW(), sg3.m_loc2, 1 );
+
+      PopEdiPointW( pMesW() );
+
+      segmGroupNum++;
+    }
+
+    segmGroupNum=0;
+    for(const Cusres::SegGr4& sg4 : cusres.m_vSegGr4)
+    {
+      SetEdiSegGr( pMesW(), SegGrElement( 4, segmGroupNum ) );
+
+      PushEdiPointW( pMesW() );
+      SetEdiPointToSegGrW( pMesW(), SegGrElement( 4, segmGroupNum ) );
+
+      SetEdiFullSegment(pMesW(), SegmElement("ERP"), sg4.m_erp.m_msgSectionCode);
+
+      int elemNum=0;
+      for(const RffElem& rffElem : sg4.m_vRff)
+        viewRffElement( pMesW(), rffElem, elemNum++);
+
+      SetEdiFullSegment(pMesW(), SegmElement("ERC"), sg4.m_erc.m_errorCode);
+
+      PopEdiPointW( pMesW() );
+
+      segmGroupNum++;
+    }
 }
 
 }//namespace TlgHandling
