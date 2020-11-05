5456def6ff8e4d622c7f2aae8b8a4986ec37e2b8
Anton Bokov <a.bokov@sirena2000.ru>
2020-11-05 17:53:32 +0300

    #38345. dcs_services refactoring - commit r234962 by vlad from svn

--- src/dcs_services.h
+++ src/dcs_services.h
@@ -2,30 +2,118 @@
 
 #include <list>
 #include <string>
-#include "astra_consts.h"
 #include <set>
+#include "astra_consts.h"
+#include "astra_types.h"
+#include "brands.h"
+#include <serverlib/cursctl.h>
 
-class DCSServiceApplyingParams
+namespace DCSServiceApplying
 {
-  public:
-    std::string airline;
-    DCSAction::Enum dcs_action;
-    std::string brand_airline;
-    std::string brand_code;
-    std::string fqt_airline;
-    std::string fqt_tier_level;
-    std::string cl;
-};
 
-class RFISCsSet : public std::set<std::string> {};
-
-class DCSServiceApplying
+class SettingsFilter
 {
   private:
-    static void addRequiredRFISCs(const DCSServiceApplyingParams& params, RFISCsSet& rfiscs);
+    mutable boost::optional<std::list<TBrand::Key>> brandKeyList;
+    mutable boost::optional<std::set<CheckIn::TPaxFQTItem>> fqts;
   public:
-    static bool isAllowed(int pax_id, DCSAction::Enum dcsAction, RFISCsSet& reqRFISCs);
-    static void throwIfNotAllowed(int pax_id, DCSAction::Enum dcsAction);
+    PaxId_t paxId;
+    AirlineCode_t airline;
+    DCSAction::Enum dcsAction;
+    Class_t cl;
+
+    SettingsFilter(const PaxId_t& paxId_,
+                   const AirlineCode_t& airline_,
+                   const DCSAction::Enum dcsAction_,
+                   const Class_t& cl_) :
+      paxId(paxId_),
+      airline(airline_),
+      dcsAction(dcsAction_),
+      cl(cl_) {}
+
+    bool suitable(const boost::optional<TBrand::Key>& brand, TBrands& brands) const;
+    bool suitable(const boost::optional<TierLevelKey>& tierLevel,
+                  const bool fqtShouldNotExists) const;
 };
+
+class Setting
+{
+  private:
+    AirlineCode_t::base_type brand_airline;
+    std::string brand_code;
+    AirlineCode_t::base_type fqt_airline;
+    std::string fqt_tier_level;
+  public:
+    int id;
+    boost::optional<TBrand::Key> brand;
+    boost::optional<TierLevelKey> tierLevel;
+    bool fqtShouldNotExists=false;
+    std::string rfisc;
+
+    bool operator < (const Setting &setting) const
+    {
+      return id<setting.id;
+    }
+
+    static const std::string& selectedFields()
+    {
+      static const std::string result=" id, brand_airline, brand_code, fqt_airline, fqt_tier_level, rfisc ";
+      return result;
+    }
+
+    static void curDef(OciCpp::CursCtl& cur, Setting& setting)
+    {
+      cur.def(setting.id)
+         .defNull(setting.brand_airline, "")
+         .defNull(setting.brand_code, "")
+         .defNull(setting.fqt_airline, "")
+         .defNull(setting.fqt_tier_level, "")
+         .def(setting.rfisc);
+    }
+
+    Setting& afterFetchProcessing()
+    {
+      if (!brand_airline.empty() && !brand_code.empty())
+        brand=boost::in_place(AirlineCode_t(brand_airline), brand_code);
+      else
+        brand=boost::none;
+
+      if (!fqt_airline.empty() && !fqt_tier_level.empty())
+        tierLevel=boost::in_place(AirlineCode_t(fqt_airline), fqt_tier_level);
+      else
+        tierLevel=boost::none;
+
+      if (fqt_airline.empty() && !fqt_tier_level.empty())
+        fqtShouldNotExists=true;
+      else
+        fqtShouldNotExists=false;
+
+      return *this;
+    }
+
+};
+
+typedef std::set<Setting> Settings;
+typedef std::pair<PaxId_t, Settings> SettingsByPaxId;
+typedef std::set<std::string> RfiscsSet;
+
+class RequiredRfiscs
+{
+  private:
+    TBrands brands;
+    std::list<std::pair<SettingsFilter, Settings>> settingsCache;
+    SettingsByPaxId settingsByPaxId;
+    DCSAction::Enum dcsAction;
+    const Settings& filterRoughly(const SettingsFilter& filter);
+    void add(const SettingsFilter& filter);
+    RfiscsSet get() const;
+  public:
+    RequiredRfiscs(const DCSAction::Enum dcsAction_,
+                   const PaxId_t& paxId);
+    bool exists() const;
+    void throwIfNotExists() const;
+};
+
+} //namespace DCSServiceApplying
 
 
