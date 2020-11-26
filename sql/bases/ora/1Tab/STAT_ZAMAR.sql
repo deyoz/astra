e602c918b6a2d8f8990012b385a927139987aa5d
den <denis_zaharov@sirena2000.ru>
2020-11-26 15:21:29 +0300

    #36649: CUWS sql

--- sql/bases/ora/1Tab/STAT_ZAMAR.sql
+++ sql/bases/ora/1Tab/STAT_ZAMAR.sql
@@ -3,5 +3,6 @@ create table stat_zamar (
     airline varchar2(3) not null,
     airp varchar2(3) not null,
     amount_ok number(9) not null,
-    amount_fault number(9) not null
+    amount_fault number(9) not null,
+    sbdo_type varchar2(10) not null
 );
