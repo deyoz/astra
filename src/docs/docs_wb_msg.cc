05b204d68b2bf660aba05316cc4b1e5ca8c4c664
Anton Bokov <a.bokov@sirena2000.ru>
2021-01-14 16:46:04 +0300

    #39579 - read WB_MSG and WB_MSG_TEXT from PG. Cherry-pick 39bfc876 from trunk

--- src/docs/docs_wb_msg.cc
+++ src/docs/docs_wb_msg.cc
@@ -1,4 +1,5 @@
 #include "docs_wb_msg.h"
+#include "pg_tquery.h"
 
 using namespace AstraLocale;
 using namespace std;
@@ -11,24 +12,24 @@ void WB_MSG(TRptParams &rpt_params, xmlNodePtr reqNode, xmlNodePtr resNode)
     xmlNodePtr dataSetsNode = NewTextChild(formDataNode, "datasets");
     xmlNodePtr dataSetNode = NewTextChild(dataSetsNode, "msg");
 
-    TCachedQuery Qry("select id, time_receive from wb_msg where point_id = :point_id and msg_type = :msg_type order by id desc",
-            QParams()
-            << QParam("point_id", otInteger, rpt_params.point_id)
-            << QParam("msg_type", otString, EncodeRptType(rpt_params.rpt_type))
-            );
-    Qry.get().Execute();
-    if(Qry.get().Eof)
+    PG::TQuery Qry;
+    Qry.SQLText = "select id, time_receive from wb_msg where point_id = :point_id and msg_type = :msg_type order by id desc";
+    Qry.CreateVariable("point_id", otInteger, rpt_params.point_id);
+    Qry.CreateVariable("msg_type", otString, EncodeRptType(rpt_params.rpt_type));
+    Qry.Execute();
+    if(Qry.Eof)
         throw UserException("MSG.NOT_DATA");
 
     xmlNodePtr rowNode = NewTextChild(dataSetNode, "row");
-    int id = Qry.get().FieldAsInteger("id");
+    int id = Qry.FieldAsInteger("id");
     // TDateTime time_receive = Qry.get().FieldAsDateTime("time_receive");
-    TCachedQuery txtQry("select text from wb_msg_text where id = :id order by page_no",
-            QParams() << QParam("id", otInteger, id));
-    txtQry.get().Execute();
+    PG::TQuery txtQry;
+    txtQry.SQLText = "select text from wb_msg_text where id = :id order by page_no";
+    txtQry.CreateVariable("id", otInteger, id);
+    txtQry.Execute();
     string text;
-    for(; not txtQry.get().Eof; txtQry.get().Next())
-        text += txtQry.get().FieldAsString("text");
+    for(; not txtQry.Eof; txtQry.Next())
+        text += txtQry.FieldAsString("text");
     NewTextChild(rowNode, "text", text);
 
     // Теперь переменные отчета
