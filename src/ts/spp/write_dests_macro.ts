42ba8df9087c85f7b68a820bdd03365a8b71b6fa
Vlad Chicherov <vlad@sirena2000.ru>
2021-02-26 15:28:57 +0300

    #39897. Transit test. Not completed

--- src/ts/spp/write_dests_macro.ts
+++ src/ts/spp/write_dests_macro.ts
@@ -187,15 +187,24 @@ $(CHANGE_SPP_FLIGHT_REQUEST $(point_dep)
 })
 
 $(defmacro NEW_SPP_FLIGHT_THREE_LEGS
-  airline
-  flt_no
-  craft
+  airline1
+  flt_no1
+  craft1
+  bort1
   airp1
   scd_out1 #формат даты: dd.mm.yyyy hh:nn
   scd_in2  #формат даты: dd.mm.yyyy hh:nn
+  airline2
+  flt_no2
+  craft2
+  bort2
   airp2
   scd_out2 #формат даты: dd.mm.yyyy hh:nn
   scd_in3  #формат даты: dd.mm.yyyy hh:nn
+  airline3
+  flt_no3
+  craft3
+  bort3
   airp3
   scd_out3 #формат даты: dd.mm.yyyy hh:nn
   scd_in4  #формат даты: dd.mm.yyyy hh:nn
@@ -213,9 +222,11 @@ $(defmacro NEW_SPP_FLIGHT_THREE_LEGS
           <dest>
             <modify/>
             <airp>$(airp1)</airp>
-            <airline>$(airline)</airline>
-            <flt_no>$(flt_no)</flt_no>
-            <craft>$(craft)</craft>
+            <airline>$(airline1)</airline>
+            <flt_no>$(flt_no1)</flt_no>
+            <craft>$(craft1)</craft>\
+$(if $(eq $(bort1) "") "" {
+            <bort>$(bort1)</bort>})
             <scd_out>$(scd_out1):00</scd_out>
             <trip_type>п</trip_type>
             <pr_tranzit>0</pr_tranzit>
@@ -224,9 +235,11 @@ $(defmacro NEW_SPP_FLIGHT_THREE_LEGS
           <dest>
             <modify/>
             <airp>$(airp2)</airp>
-            <airline>$(airline)</airline>
-            <flt_no>$(flt_no)</flt_no>
-            <craft>$(craft)</craft>
+            <airline>$(airline2)</airline>
+            <flt_no>$(flt_no2)</flt_no>
+            <craft>$(craft2)</craft>\
+$(if $(eq $(bort2) "") "" {
+            <bort>$(bort2)</bort>})
             <scd_in>$(scd_in2):00</scd_in>
             <scd_out>$(scd_out2):00</scd_out>
             <trip_type>п</trip_type>
@@ -236,9 +249,11 @@ $(defmacro NEW_SPP_FLIGHT_THREE_LEGS
           <dest>
             <modify/>
             <airp>$(airp3)</airp>
-            <airline>$(airline)</airline>
-            <flt_no>$(flt_no)</flt_no>
-            <craft>$(craft)</craft>
+            <airline>$(airline3)</airline>
+            <flt_no>$(flt_no3)</flt_no>
+            <craft>$(craft3)</craft>\
+$(if $(eq $(bort3) "") "" {
+            <bort>$(bort3)</bort>})
             <scd_in>$(scd_in3):00</scd_in>
             <scd_out>$(scd_out3):00</scd_out>
             <trip_type>п</trip_type>
