78800aec1dbb63c30f1de406d0b5bfd640ec89fa
Maxim Surikov <m.surikov@sirena2000.ru>
2021-05-12 15:14:42 +0300

    #40282 PG: CRS_PAX_TKN: fix TSimplePaxItem::getCrsByPaxId

--- src/passenger.cc
+++ src/passenger.cc
@@ -2453,9 +2453,15 @@ bool TSimplePaxItem::getCrsByPaxId(PaxId_t pax_id, bool skip_deleted)
   QParams QryParams;
   QryParams << QParam("pax_id", otInteger, pax_id.get());
   DB::TCachedQuery PaxQry(
-        PgOra::getROSession("CRS_PAX"),
-        "SELECT * FROM crs_pax "
-        "WHERE pax_id=:pax_id "
+        PgOra::getROSession("CRS_PAX-CRS_PNR"),
+        "SELECT crs_pax.* "
+        + CheckIn::TSimplePaxItem::origSubclassFromCrsSQL() + " AS subclass, "
+        + CheckIn::TSimplePaxItem::cabinSubclassFromCrsSQL() + " AS cabin_subclass, "
+        + CheckIn::TSimplePaxItem::cabinClassFromCrsSQL() + " AS cabin_class, "
+        "FROM crs_pax, crs_pnr "
+        "WHERE crs_pax.pax_id=:pax_id "
+        "AND crs_pax.pnr_id=crs_pnr.pnr_id "
+        "AND crs_pnr.system='CRS' "
         + std::string(skip_deleted ? "AND pr_del=0 " : ""),
         QryParams, STDLOG);
   PaxQry.get().Execute();
