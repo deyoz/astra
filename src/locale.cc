116ec4d72aa432aaa782dfa1924f0bcc85930a9e
Felix <f.tsaruev@sirena2000.ru>
2021-05-24 15:29:42 +0300

    #40389 fix err dbo init

--- src/locale.cc
+++ src/locale.cc
@@ -146,6 +146,7 @@ int init_locale(void)
     init_pnr_callbacks();
     init_rfisc_callbacks();
     initPassengerCallbacks();
+    dbo::initStructures();
     APPS::init_callbacks();
     IAPI::init_callbacks();
     PaxCalcData::init_callbacks();
