da2ca4b4966b55f364f84c562062d526aaf603a8
Vlad Chicherov <vlad@sirena2000.ru>
2021-05-27 03:05:45 +0300

    #40698. Tests for mobile payment. 44f8ab6fe, e028b0cd6 from TRUNK

--- src/ts/spp/write_trips_macro.ts
+++ src/ts/spp/write_trips_macro.ts
@@ -33,10 +33,13 @@ $(defmacro PREP_CHECKIN
 
 $(defmacro OPEN_CHECKIN
   point_id
-  act="$(date_format %d.%m.%Y) $(date_format %H:%M)"
+  act=$(date_format {%d.%m.%Y %H:%M})
+  stage_id1
+  stage_id2
 {
+
 !! capture=on
-{<?xml version='1.0' encoding='CP866'?>
+<?xml version='1.0' encoding='CP866'?>
 <term>
   <query handle='0' id='sopp' ver='1' opr='PIKE' screen='SOPP.EXE' mode='STAND' lang='RU' term_id='2479792165'>
     <WriteTrips>
@@ -45,24 +48,130 @@ $(defmacro OPEN_CHECKIN
           <point_id>$(point_id)</point_id>
           <tripstages>
             <stage>
-              <stage_id>10</stage_id>
+              <stage_id>$(stage_id1)</stage_id>
               <act>$(act):00</act>
-            </stage>
+            </stage>\
+$(if $(eq $(stage_id2) "") "" {
             <stage>
-              <stage_id>20</stage_id>
+              <stage_id>$(stage_id2)</stage_id>
               <act>$(act):00</act>
-            </stage>
+            </stage>})
           </tripstages>
         </trip>
       </trips>
     </WriteTrips>
   </query>
-</term>}
+</term>
 
 >> lines=auto
     <command>
       <message lexema_id='MSG.DATA_SAVED' code='0'>...</message>
     </command>
+
+}) #end-of-macro
+
+#########################################################################################
+
+$(defmacro SET_STAGE
+  point_id
+  act=$(date_format {%d.%m.%Y %H:%M})
+  stage_id1
+  stage_id2
+{
+
+!! capture=on
+<?xml version='1.0' encoding='CP866'?>
+<term>
+  <query handle='0' id='sopp' ver='1' opr='PIKE' screen='SOPP.EXE' mode='STAND' lang='RU' term_id='2479792165'>
+    <WriteTrips>
+      <trips>
+        <trip>
+          <point_id>$(point_id)</point_id>
+          <tripstages>
+            <stage>
+              <stage_id>$(stage_id1)</stage_id>
+              <act>$(act):00</act>
+            </stage>\
+$(if $(eq $(stage_id2) "") "" {
+            <stage>
+              <stage_id>$(stage_id2)</stage_id>
+              <act>$(act):00</act>
+            </stage>})
+          </tripstages>
+        </trip>
+      </trips>
+    </WriteTrips>
+  </query>
+</term>
+
+>> lines=auto
+    <command>
+      <message lexema_id='MSG.DATA_SAVED' code='0'>...</message>
+    </command>
+
+}) #end-of-macro
+
+#########################################################################################
+
+$(defmacro CANCEL_STAGE
+  point_id
+  act=$(date_format {%d.%m.%Y %H:%M})
+  stage_id1
+  stage_id2
+{
+
+!! capture=on
+<?xml version='1.0' encoding='CP866'?>
+<term>
+  <query handle='0' id='sopp' ver='1' opr='PIKE' screen='SOPP.EXE' mode='STAND' lang='RU' term_id='2479792165'>
+    <WriteTrips>
+      <trips>
+        <trip>
+          <point_id>$(point_id)</point_id>
+          <tripstages>
+            <stage>
+              <stage_id>$(stage_id1)</stage_id>
+              <old_act>$(act):00</old_act>
+            </stage>\
+$(if $(eq $(stage_id2) "") "" {
+            <stage>
+              <stage_id>$(stage_id2)</stage_id>
+              <old_act>$(act):00</old_act>
+            </stage>})
+          </tripstages>
+        </trip>
+      </trips>
+    </WriteTrips>
+  </query>
+</term>
+
+>> lines=auto
+    <command>
+      <message lexema_id='MSG.DATA_SAVED' code='0'>...</message>
+    </command>
+
+}) #end-of-macro
+
+#########################################################################################
+
+
+$(defmacro PREP_CHECKIN
+  point_id
+  act="$(date_format %d.%m.%Y) $(date_format %H:%M)"
+{
+
+$(SET_STAGE $(point_id) $(act) 10)
+
+}) #end-of-macro
+
+#########################################################################################
+
+$(defmacro OPEN_CHECKIN
+  point_id
+  act=$(date_format {%d.%m.%Y %H:%M})
+{
+
+$(SET_STAGE $(point_id) $(act) 10 20)
 
 }) #end-of-macro
 
@@ -72,30 +181,8 @@ $(defmacro CLOSE_CHECKIN
   point_id
   act="$(date_format %d.%m.%Y) $(date_format %H:%M)"
 {
-!! capture=on
-{<?xml version='1.0' encoding='CP866'?>
-<term>
-  <query handle='0' id='sopp' ver='1' opr='PIKE' screen='SOPP.EXE' mode='STAND' lang='RU' term_id='2479792165'>
-    <WriteTrips>
-      <trips>
-        <trip>
-          <point_id>$(point_id)</point_id>
-          <tripstages>
-            <stage>
-              <stage_id>30</stage_id>
-              <act>$(act):00</act>
-            </stage>
-          </tripstages>
-        </trip>
-      </trips>
-    </WriteTrips>
-  </query>
-</term>}
 
->> lines=auto
-    <command>
-      <message lexema_id='MSG.DATA_SAVED' code='0'>...</message>
-    </command>
+$(SET_STAGE $(point_id) $(act) 30)
 
 }) #end-of-macro
 
@@ -105,31 +192,77 @@ $(defmacro CLOSE_CHECKIN_CANCEL
   point_id
   act="$(date_format %d.%m.%Y) $(date_format %H:%M)"
 {
-!! capture=on
-{<?xml version='1.0' encoding='CP866'?>
-<term>
-  <query handle='0' id='sopp' ver='1' opr='PIKE' screen='SOPP.EXE' mode='STAND' lang='RU' term_id='2479792165'>
-    <WriteTrips>
-      <trips>
-        <trip>
-          <point_id>$(point_id)</point_id>
-          <tripstages>
-            <stage>
-              <stage_id>30</stage_id>
-              <old_act>$(act):00</old_act>
-            </stage>
-          </tripstages>
-        </trip>
-      </trips>
-    </WriteTrips>
-  </query>
-</term>}
 
->> lines=auto
-    <command>
-      <message lexema_id='MSG.DATA_SAVED' code='0'>...</message>
-    </command>
+$(CANCEL_STAGE $(point_id) $(act) 30)
 
 }) #end-of-macro
 
 #########################################################################################
+
+$(defmacro OPEN_WEB_CHECKIN
+  point_id
+  act="$(date_format %d.%m.%Y) $(date_format %H:%M)"
+{
+
+$(SET_STAGE $(point_id) $(act) 25)
+
+}) #end-of-macro
+
+#########################################################################################
+
+$(defmacro CLOSE_WEB_CHECKIN
+  point_id
+  act="$(date_format %d.%m.%Y) $(date_format %H:%M)"
+{
+
+$(SET_STAGE $(point_id) $(act) 35)
+
+}) #end-of-macro
+
+#########################################################################################
+
+$(defmacro CLOSE_WEB_CHECKIN_CANCEL
+  point_id
+  act="$(date_format %d.%m.%Y) $(date_format %H:%M)"
+{
+
+$(CANCEL_STAGE $(point_id) $(act) 35)
+
+}) #end-of-macro
+
+#########################################################################################
+
+$(defmacro OPEN_BOARDING
+  point_id
+  act="$(date_format %d.%m.%Y) $(date_format %H:%M)"
+{
+
+$(SET_STAGE $(point_id) $(act) 40)
+
+}) #end-of-macro
+
+#########################################################################################
+
+$(defmacro CLOSE_BOARDING
+  point_id
+  act="$(date_format %d.%m.%Y) $(date_format %H:%M)"
+{
+
+$(SET_STAGE $(point_id) $(act) 50)
+
+}) #end-of-macro
+
+#########################################################################################
+
+$(defmacro CLOSE_BOARDING_CANCEL
+  point_id
+  act="$(date_format %d.%m.%Y) $(date_format %H:%M)"
+{
+
+$(CANCEL_STAGE $(point_id) $(act) 50)
+
+}) #end-of-macro
+
+#########################################################################################
+
+
