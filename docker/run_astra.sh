8ae9001a81e8464069bb59f96e1a31ceff2dc73a
Roman Kovalev <rom@sirena2000.ru>
2021-06-23 14:49:58 +0300

    #40453 - docker compose no oracle build. auto deploy

--- docker/run_astra.sh
+++ docker/run_astra.sh
@@ -1,10 +1,11 @@
 #!/bin/sh
 
-echo "Updating executables & libs..."
+echo "Updating executables & libs & demo_local_after.tcl ..."
 cp /opt/astra/src/astra /opt/astra/run \
     && cp /opt/astra/locallibs/serverlib/src/dispatcher /opt/astra/run \
     && cp /opt/astra/locallibs/serverlib/src/supervisor /opt/astra/run \
     && cp /opt/astra/locallibs/serverlib/*tcl /opt/astra/run \
+    && cp /opt/astra/src/demo_local_after.tcl /opt/astra/run \
     && mkdir /opt/astra/run/libs && find ./externallibs -maxdepth 3 -name '*.so*' -exec cp '{}' /opt/astra/run/libs/ \;
 echo "OK"
 
