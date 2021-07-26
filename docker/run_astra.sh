0343ca209cbcc878cf052ed011d1f3f434f222be
Roman Kovalev <rom@sirena2000.ru>
2021-07-26 16:20:26 +0300

    #40453 - update libs on startup fix

--- docker/run_astra.sh
+++ docker/run_astra.sh
@@ -6,7 +6,7 @@ cp /opt/astra/src/astra /opt/astra/run \
     && cp /opt/astra/locallibs/serverlib/src/supervisor /opt/astra/run \
     && cp /opt/astra/locallibs/serverlib/*tcl /opt/astra/run \
     && cp /opt/astra/docker/RUN_EXAMPLE/demo_local_after.tcl /opt/astra/run \
-    && mkdir /opt/astra/run/libs && find ./externallibs -maxdepth 3 -name '*.so*' -exec cp '{}' /opt/astra/run/libs/ \;
+    && mkdir -p /opt/astra/run/libs && find ./externallibs -maxdepth 3 -name '*.so*' -exec cp '{}' /opt/astra/run/libs/ \;
 echo "OK"
 
 echo "Starting astra!"
