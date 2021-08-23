8bc320033c4ace09aea1a59e2c9cfefdd464eed5
Anton Bokov <a.bokov@sirena2000.ru>
2021-08-23 11:32:45 +0300

    #40643 - remove extra code

--- src/report.h
+++ src/report.h
@@ -10,31 +10,4 @@ namespace ASTRA {
 
 std::optional<AirportCode_t> get_last_trfer_airp(const GrpId_t& grpId);
 
-
-//std::vector<Dates::DateTime_t> get_airline_period_first_dates(const AirlineCode_t& airl);
-
-//std::optional<Dates::DateTime_t> get_airline_period_last_date(const AirlineCode_t& airl,
-//                                                              const Dates::DateTime_t& periodFirstDate);
-
-//std::vector<Dates::time_period> get_airline_periods(const AirlineCode_t& airl,
-//                                                    const Dates::DateTime_t& firstDate,
-//                                                    const Dates::DateTime_t& lastDate);
-
-//std::vector<AirlineCode_t> get_airline_list(const AirportCode_t& airp,
-//                                            const Dates::time_period& period);
-
-
-
-//std::vector<Dates::DateTime_t> get_airport_period_first_dates(const AirportCode_t& airp);
-
-//std::optional<Dates::DateTime_t> get_airport_period_last_date(const AirportCode_t& airp,
-//                                                              const Dates::DateTime_t& periodFirstDate);
-
-//std::vector<Dates::time_period> get_airport_periods(const AirportCode_t& airp,
-//                                                    const Dates::DateTime_t& firstDate,
-//                                                    const Dates::DateTime_t& lastDate);
-
-//std::vector<AirportCode_t> get_airport_list(const AirlineCode_t& airl,
-//                                            const Dates::time_period& period);
-
 }//namespace ASTRA
